024b3a00f2bd025c64ab051e010a6f00f6a9c6f3
jbernhard <bernhard@fortiss.org>
2019-04-03 17:11:48 +0200

    setup basic scenario generation architecture

---
+++ modules/runtime/scenario/scenario.py
@@ -0,0 +1,55 @@
+
+
+
+
+
+
+
+
+
+class Scenario:
+    def __init__(self, world_state=None, description=None):
+        self.world_state = world_state
+        self.description = description
+        self.goal_criteria = []
+
+    def set_world_state(self, world_state):
+        """ sets initial world state of this scenario
+        
+        Arguments:
+            world_state {[bark.world.World]} -- [pass a full world instance giving the actual world state]
+        """
+
+        self.world_state = world_state
+
+    def get_world_state(self):
+        """get initial world state of scenario to start simulation from here
+        
+        Returns:
+            [barl.world.World] 
+        """
+        return self.world_state
+
+    def set_description(description):
+        """set scenario description
+        
+        Arguments:
+            description {[dict]} -- [pass a dictionary with type and other info of this scenario....to be defined what it must contain]
+        """
+
+    def get_description():
+        return self.description
+
+    def add_goal_criterion(goal_criterion):
+        """add a goal specification to this scenario, e.g. drive into a certain goal region, or maximum amount of time 10s, multiple criteria can be added subsequently
+        
+        Arguments:
+            goal_criterion {[todo]} -- [todo]
+        """
+
+        self.goal_criteria.append(goal_criterion)
+
+
+
+
+
