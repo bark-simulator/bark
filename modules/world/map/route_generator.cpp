c784d9c8c5ebfacb887c4bb84741c665e04df324
jbernhard <bernhard@fortiss.org>
2019-04-03 17:10:50 +0200

    implement get_driving_corridor function in map_interface

--- modules/world/map/route_generator.cpp
+++ modules/world/map/route_generator.cpp
@@ -22,60 +22,13 @@ bool RouteGenerator::generate(Point2d point,
     map_interface_->get_nearest_lanes(point, 1, lanes);
     LanePtr current_lane = lanes.at(0);
 
-    std::cout << "Nearest lane_id: " << current_lane->get_id() << std::endl;
-
-    // TODO: with parameter --> length
-    std::pair< std::vector<LanePtr>, std::vector<LanePtr> > route =
-      map_interface_->get_lane_boundary_horizon(current_lane->get_id(),
-                                                goal_lane_id_);
-
-
-    if (route.first[0]) {
-      inner_line_ = route.first[0]->get_line();
-      // inner lane
-      for (int i = 1; i < route.first.size(); i++) {
-        if (route.first[i] != NULL) {
-          inner_line_.concatenate_linestring(route.first[i]->get_line());
-        }
-      }
-    }
-    if (route.second[0]) {
-      outer_line_ = route.second[0]->get_line();
-      // inner lane
-      for (int i = 1; i < route.second.size(); i++) {
-        if (route.second[i] != NULL) {
-          outer_line_.concatenate_linestring(route.second[i]->get_line());
-        }
-      }
-    }
-
-    if (route.first[0] != NULL && route.second[0] != NULL) {
-      center_line_ = calculate_center_line();
-      computed_ = true;
-    } else {
-      return false;
-    }
+    return map_interface_->get_driving_corridor(current_lane->get_id(), goal_lane_id_,
+                           inner_line_, outer_line_, center_line_  );
   }
-  return true;
-}
-
-Line RouteGenerator::calculate_center_line(double horizon) {
-  // select line with more points for looping to avoid resolution reduction
-  Line line_more_points = outer_line_;
-  Line line_less_points = inner_line_;
-  if ( inner_line_.obj_.size() > outer_line_.obj_.size() ) {
-    line_more_points = inner_line_;
-    line_less_points = outer_line_;
+  else {
+    return true;
   }
-  // TODO(@hart): use horizon
-  for ( Point2d& point_loop : line_more_points.obj_ ) {
-    Point2d nearest_point_other = geometry::get_nearest_point(line_less_points,
-                                                              point_loop);
-    geometry::Point2d middle_point = (point_loop + nearest_point_other) / 2;
-    center_line_.add_point(middle_point);
-  }
-
-  return center_line_;
+    
 }
 
 }  // namespace map
