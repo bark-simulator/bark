d7b24b0348d86ab6b59dcc2319f412675bf92707
jbernhard <bernhard@fortiss.org>
2019-04-08 16:26:58 +0200

    change to new modules names

--- modules/runtime/scenario/scenario_generation/model_json_conversion.py
+++ modules/runtime/scenario/scenario_generation/model_json_conversion.py
@@ -1,36 +1,38 @@
-from bark._bark._bark import *
-from bark._bark._bark.geometry.standard_shapes import *
-from bark._bark._bark.world.agent import *
-from bark._bark._bark.world.behavior import *
-from bark._bark._bark.world.world import *
-from bark._bark._bark.world import *
-from bark._bark._bark.dynamic import *
-from bark._bark._bark.execution import *
-from bark.commons.params import ParameterServer
-
+from bark.world.agent import *
+from bark.models.behavior import *
+from bark.world import *
+from bark.world.map import *
+from bark.models.dynamic import *
+from bark.models.execution import *
+from bark.geometry import *
+from bark.geometry.standard_shapes import *
+from modules.runtime.commons.parameters import ParameterServer
 import inspect
 import re
 import sys
 import numpy as np
 
 
-
 class ModelJsonConversion:
-     def __init__(self):
-        self.model_list_behavior = self.extract_models("bark._bark._bark.environment.behavior", "Behavior\w+")
-        self.model_list_dynamic = self.extract_models("bark._bark._bark.dynamic", "SingleTrack")
-        self.model_list_execution = self.extract_models("bark._bark._bark.execution", "ExecutionModel\w+")
-        self.model_list_shape =  self.extract_models("bark._bark._bark.environment.geometry.standard_shapes", "\w+")
+    def __init__(self):
+        self.model_list_behavior = self.extract_models(
+            "bark.models.behavior", "Behavior\w+")
+        self.model_list_dynamic = self.extract_models(
+            "bark.models.dynamic", "SingleTrack")
+        self.model_list_execution = self.extract_models(
+            "bark.models.execution", "ExecutionModel\w+")
+        self.model_list_shape = self.extract_models(
+            "bark.geometry.standard_shapes", "\w+")
 
-    def agent_from_json(self, dict_state, agent_json, param_server): 
+    def agent_from_json(self, dict_state, agent_json, param_server):
         bark_agent = Agent(np.array(agent_setting["state"]), 
-            self.convert_model(agent_json["behavior_model"],self.param_server), 
-            self.convert_model(agent_json["dynamic_model"],None),
-            self.convert_model(agent_json["execution_model"],self.param_server), 
-            Polygon2d(agent_json["shape"]["center_pose"],
-                    np.array(agent_json["shape"]["polygon_points"])),
-                param_server.addChild("agent"), Model3d(eval(agent_setting["model_type"])))
-            bark_agent.id = agent_json["id"]
+        self.convert_model(agent_json["behavior_model"],self.param_server), 
+        self.convert_model(agent_json["dynamic_model"],None),
+        self.convert_model(agent_json["execution_model"],self.param_server), 
+        Polygon2d(agent_json["shape"]["center_pose"],
+                np.array(agent_json["shape"]["polygon_points"])),
+            param_server.addChild("agent"), Model3d(eval(agent_setting["model_type"])))
+        bark_agent.id = agent_json["id"]
         return bark_agent
 
     def agent_to_json(self, agent):
