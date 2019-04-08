79abe9ecdefdcbab306d4dab2edbbd20c3700b43
Klemens Esterle <esterle@fortiss.org>
2019-04-08 16:58:25 +0200

    introducing collision checker class

---
+++ modules/world/collision/collision_checker_agents.cpp
@@ -0,0 +1,21 @@
+// Copyright (c) 2019 fortiss GmbH, Julian Bernhard, Klemens Esterle, Patrick Hart, Tobias Kessler
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
+
+#include "modules/world/collision/collision_checker_agents.hpp"
+#include "modules/world/world.hpp"
+
+namespace modules
+{
+namespace world
+{
+namespace collision
+{
+  bool CollisioncheckerAgents::checkCollision(const world::World& observed_world) const {
+    return false;
+  };
+
+}
+}
+}
--- modules/world/world.hpp
+++ modules/world/world.hpp
@@ -13,6 +13,7 @@
 #include "modules/world/map/roadgraph.hpp"
 #include "modules/world/objects/agent.hpp"
 #include "modules/world/objects/object.hpp"
+#include "modules/world/collision/base_collision_checker.hpp"
 
 namespace modules {
 namespace world {
@@ -20,6 +21,7 @@ namespace world {
 using world::objects::AgentId;
 using world::objects::AgentPtr;
 using world::objects::ObjectPtr;
+using world::collision::CollisionCheckerPtr;
 
 typedef std::unordered_map<AgentId, AgentPtr> AgentMap;
 typedef std::unordered_map<AgentId, ObjectPtr> ObjectMap;
@@ -36,8 +38,11 @@ class World : public commons::BaseType {
   AgentMap get_agents() const { return agents_; }
   AgentPtr get_agent(AgentId id) const { return agents_.at(id); }
   ObjectMap get_objects() const { return objects_; }
+  CollisionCheckerPtr get_collision_checker() const { return collision_checker_; }
 
   void set_map(const world::map::MapInterfacePtr& map) { map_ = map; }
+  void set_collision_checker(const CollisionCheckerPtr& cchecker) { collision_checker_ = cchecker; }
+  
   void add_agent(const AgentPtr& agent);
   void add_object(const ObjectPtr& agent);
 
@@ -49,8 +54,6 @@ class World : public commons::BaseType {
   }
 
   bool CheckCollision() const;
-  bool CheckCollisionWithRoad() const;
-  bool CheckCollisionWithAgents() const;
 
   bool Valid() const;
   void Step(float delta_time);
@@ -62,6 +65,7 @@ class World : public commons::BaseType {
   world::map::MapInterfacePtr map_;
   AgentMap agents_;
   ObjectMap objects_;
+  CollisionCheckerPtr collision_checker_;
   double world_time_;
 };
 
