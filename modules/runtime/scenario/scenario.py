83535de65d730eff42a287510eeb164684f6d987
jbernhard <bernhard@fortiss.org>
2019-04-09 17:03:13 +0200

    add license headers

--- modules/runtime/scenario/scenario.py
+++ modules/runtime/scenario/scenario.py
@@ -1,11 +1,7 @@
-
-
-
-
-
-
-
-
+# Copyright (c) 2019 fortiss GmbH
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
 
 class Scenario:
     def __init__(self, world_state=None, description=None):
