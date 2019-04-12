c6063b84068eb8a09e4750cbdbfd7d032271da2a
jbernhard <bernhard@fortiss.org>
2019-04-12 16:59:10 +0200

    add goal lane id to scenario generation

--- examples/od8_scen_gen_uniform.py
+++ examples/od8_scen_gen_uniform.py
@@ -15,21 +15,21 @@ import time
 
 param_server = ParameterServer()
 
-viewer = MPViewer(params=param_server)
+viewer = PygameViewer(params=param_server, x_range=[-100, 100], y_range=[-100, 100])
 
 sim_step_time = param_server["simulation"]["step_time",
                                         "Step-time used in simulation",
-                                        0.05]
+                                        1]
 sim_real_time_factor = param_server["simulation"]["real_time_factor",
                                                 "execution in real-time or faster",
                                                 1]
-scenario_generation = UniformVehicleDistribution(num_scenarios=1, random_seed=0, params=param_server)
+scenario_generation = UniformVehicleDistribution(num_scenarios=3, random_seed=0, params=param_server)
 
 
 for _ in range(0,5): # run 5 scenarios in a row, repeating after 3
     scenario = scenario_generation.get_next_scenario()
     for _ in range(0, 3): # run each scenario for 3 steps
-        #scenario.world_state.step(sim_step_time)
+        scenario.world_state.step(sim_step_time)
         viewer.drawWorld(scenario.world_state)
-        viewer.show(block=True)
+        viewer.show(block=False)
         time.sleep(sim_step_time/sim_real_time_factor)
