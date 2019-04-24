6b841cf171c539cb50ee8e400477623eda813c14
jbernhard <bernhard@fortiss.org>
2019-04-24 17:14:14 +0200

    agent pickle unpickle test with first equal asserts

--- modules/world/objects/object.hpp
+++ modules/world/objects/object.hpp
@@ -36,12 +36,14 @@ class Object : public commons::BaseType {
 
   AgentId get_agent_id() { return agent_id_; }
 
+  void set_agent_id(const AgentId& agent_id) { agent_id_ = agent_id;}
+
   virtual Object *Clone() const;
 
  private:
   geometry::Polygon shape_;
   geometry::Model3D model_3d_;
-  const AgentId agent_id_;
+  AgentId agent_id_;
 
   static AgentId agent_count; 
 };
