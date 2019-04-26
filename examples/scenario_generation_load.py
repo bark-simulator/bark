a97ed52cfd0114578854a041994966fc96ce27d3
jbernhard <bernhard@fortiss.org>
2019-04-26 17:01:01 +0200

    add example for scenario dumping and loading

--- examples/scenario_generation_load.py
+++ examples/scenario_generation_load.py
@@ -3,10 +3,7 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-
-
-
-from modules.runtime.scenario.scenario_generation.uniform_vehicle_distribution import UniformVehicleDistribution
+from modules.runtime.scenario.scenario_generation.scenario_generation import ScenarioGeneration
 from modules.runtime.viewer.pygame_viewer import PygameViewer
 from modules.runtime.viewer.matplotlib_viewer import MPViewer
 from modules.runtime.commons.parameters import ParameterServer
@@ -15,7 +12,7 @@ import time
 
 param_server = ParameterServer()
 
-viewer = PygameViewer(params=param_server, x_range=[-100, 100], y_range=[-100, 100])
+viewer = PygameViewer(params=param_server, x_range=[-200,0], y_range=[-100,100])
 
 sim_step_time = param_server["simulation"]["step_time",
                                         "Step-time used in simulation",
@@ -23,13 +20,15 @@ sim_step_time = param_server["simulation"]["step_time",
 sim_real_time_factor = param_server["simulation"]["real_time_factor",
                                                 "execution in real-time or faster",
                                                 1]
-scenario_generation = UniformVehicleDistribution(num_scenarios=3, random_seed=0, params=param_server)
+scenario_generation = ScenarioGeneration()
+scenario_generation.load_scenario_list(filename="examples/scenarios/test.bark_scenarios")
 
 
 for _ in range(0,5): # run 5 scenarios in a row, repeating after 3
     scenario = scenario_generation.get_next_scenario()
+    world_state = scenario.get_world_state()
     for _ in range(0, 3): # run each scenario for 3 steps
-        scenario.world_state.step(sim_step_time)
-        viewer.drawWorld(scenario.world_state)
+        world_state.step(sim_step_time)
+        viewer.drawWorld(world_state)
         viewer.show(block=False)
         time.sleep(sim_step_time/sim_real_time_factor)
