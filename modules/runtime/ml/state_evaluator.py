732729d2d65b3555bc2c767d61b06da226253a57
juloberno <juloberno@gmail.com>
2019-06-03 22:31:44 +0200

    start with state evaluator

--- modules/runtime/ml/state_evaluator.py
+++ modules/runtime/ml/state_evaluator.py
@@ -1,12 +1,19 @@
-
+from bark.world.evaluation import EvaluatorGoalReached
 
 
 class StateEvaluator:
     def get_evaluation(self, world):
         return # reward, done, info
 
+    def add_world_evaluators(self, world)
+        return # world
+
 
 class GoalReached(StateEvaluator):
     def get_evaluation(self, world):
+
+    def add_world_evaluators(self, world):
+        evaluator = EvaluatorGoalReached(agent.id)
+        world.add_evaluator("success", evaluator)
         
 
