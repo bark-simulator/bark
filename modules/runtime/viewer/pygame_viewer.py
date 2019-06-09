a5c8593dfb42a5c56d82e84d1876fdee53978c8c
jbernhard <bernhard@fortiss.org>
2019-06-09 17:12:43 +0200

    move dynamic range to main viewer and add eval agent tracking

--- modules/runtime/viewer/pygame_viewer.py
+++ modules/runtime/viewer/pygame_viewer.py
@@ -13,17 +13,11 @@ from modules.runtime.viewer.viewer import BaseViewer
 
 class PygameViewer(BaseViewer):
     def __init__(self, params=None, **kwargs):
-        super(PygameViewer, self).__init__(params=params)
-        self.world_x_range = kwargs.pop("x_range", [-40, 40])
-        self.world_y_range = kwargs.pop("y_range", [-40, 40])
-        self.use_world_bounds = kwargs.pop("use_world_bounds", False)
-        self.follow_agent_id = kwargs.pop("follow_agent_id", None)
+        super(PygameViewer, self).__init__(params=params, **kwargs)
+
         self.screen_dims = kwargs.pop("screen_dims", [1024, 1024])
         self.screen_width = self.screen_dims[0]
         self.screen_height = self.screen_dims[1]
-
-        self.dynamic_world_x_range = self.world_x_range.copy()
-        self.dynamic_world_y_range = self.world_y_range.copy()
 
         try:
             self.screen = pg.display.set_mode((self.screen_width,
@@ -35,40 +29,6 @@ class PygameViewer(BaseViewer):
         except pg.error:
             self.screen = None
             print("No available video device")
-
-    def _update_world_view_range(self, world):
-        if self.follow_agent_id is not None:
-            follow_agent = world.agents[self.follow_agent_id]
-            state = follow_agent.followed_trajectory[-1,:]
-            pose = np.zeros(3)
-            pose[0] = state[int(StateDefinition.X_POSITION)]
-            pose[1] = state[int(StateDefinition.Y_POSITION)]
-            pose[2] = state[int(StateDefinition.THETA_POSITION)]
-
-            # center range on agents coordinates
-            self.dynamic_world_x_range[0] = pose[0] + self.world_x_range[0]
-            self.dynamic_world_x_range[1] = pose[0] + self.world_x_range[1]
-
-            self.dynamic_world_y_range[0] = pose[1] + self.world_y_range[0]
-            self.dynamic_world_y_range[1] = pose[1] + self.world_y_range[1]
-
-        if self.use_world_bounds:
-            bb = world.bounding_box
-            self.dynamic_world_x_range = [bb[0].x(), bb[1].x()]
-            self.dynamic_world_y_range = [bb[0].y(), bb[1].y()]
-
-            diffx = abs(self.dynamic_world_x_range[1] - self.dynamic_world_x_range[0])
-            diffy = abs(self.dynamic_world_y_range[1] - self.dynamic_world_y_range[0])
-
-            # enforce that in both dimensions  the same range is covered
-            if diffx > diffy: 
-                self.dynamic_world_y_range[0] -= (diffx - diffy)/2
-                self.dynamic_world_y_range[1] += (diffx - diffy)/2
-            else:
-                self.dynamic_world_x_range[0] -= (diffy - diffx)/2
-                self.dynamic_world_x_range[1] += (diffy - diffx)/2
-
-
 
     def drawPoint2d(self, point2d, color, alpha):
         if self.screen is None:
@@ -130,7 +90,6 @@ class PygameViewer(BaseViewer):
 
     def drawWorld(self, world, eval_agent_ids=None):
         self.clear()
-        self._update_world_view_range(world)
         super(PygameViewer, self).drawWorld(world, eval_agent_ids)
         self.show()
 
