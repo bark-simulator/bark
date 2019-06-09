b4c58e165dabe0bace078e820c3001205156e8f0
jbernhard <bernhard@fortiss.org>
2019-06-09 17:12:43 +0200

    move dynamic range to main viewer and add eval agent tracking

--- modules/runtime/viewer/viewer.py
+++ modules/runtime/viewer/viewer.py
@@ -10,7 +10,7 @@ from bark.models.dynamic import *
 
 
 class BaseViewer(Viewer):
-    def __init__(self, params=None):
+    def __init__(self, params=None, **kwargs):
         Viewer.__init__(self)
         # color parameters
         # agents
@@ -27,6 +27,51 @@ class BaseViewer(Viewer):
 
 
         self.parameters = params
+
+        self.world_x_range = kwargs.pop("x_range", [-40, 40])
+        self.world_y_range = kwargs.pop("y_range", [-40, 40])
+        self.use_world_bounds = kwargs.pop("use_world_bounds", False)
+        self.follow_agent_id = kwargs.pop("follow_agent_id", None)
+
+        self.dynamic_world_x_range = self.world_x_range.copy()
+        self.dynamic_world_y_range = self.world_y_range.copy()
+
+    def _update_world_view_range(self, world, eval_agent_ids=None):
+        if self.follow_agent_id is not None:
+            if isinstance(self.follow_agent_id, bool) and \
+                     eval_agent_ids is not None and \
+                     len(eval_agent_ids) == 1:
+                follow_agent =world.agents[eval_agent_ids[0]]
+            else:
+                follow_agent = world.agents[self.follow_agent_id]
+            state = follow_agent.state
+            pose = np.zeros(3)
+            pose[0] = state[int(StateDefinition.X_POSITION)]
+            pose[1] = state[int(StateDefinition.Y_POSITION)]
+            pose[2] = state[int(StateDefinition.THETA_POSITION)]
+
+            # center range on agents coordinates
+            self.dynamic_world_x_range[0] = pose[0] + self.world_x_range[0]
+            self.dynamic_world_x_range[1] = pose[0] + self.world_x_range[1]
+
+            self.dynamic_world_y_range[0] = pose[1] + self.world_y_range[0]
+            self.dynamic_world_y_range[1] = pose[1] + self.world_y_range[1]
+
+        if self.use_world_bounds:
+            bb = world.bounding_box
+            self.dynamic_world_x_range = [bb[0].x(), bb[1].x()]
+            self.dynamic_world_y_range = [bb[0].y(), bb[1].y()]
+
+            diffx = abs(self.dynamic_world_x_range[1] - self.dynamic_world_x_range[0])
+            diffy = abs(self.dynamic_world_y_range[1] - self.dynamic_world_y_range[0])
+
+            # enforce that in both dimensions  the same range is covered
+            if diffx > diffy: 
+                self.dynamic_world_y_range[0] -= (diffx - diffy)/2
+                self.dynamic_world_y_range[1] += (diffx - diffy)/2
+            else:
+                self.dynamic_world_x_range[0] -= (diffy - diffx)/2
+                self.dynamic_world_x_range[1] += (diffy - diffx)/2
 
     def drawPoint2d(self, point2d, color, alpha):
         pass
@@ -57,6 +102,7 @@ class BaseViewer(Viewer):
             self.drawAgent(agent)
 
     def drawWorld(self, world, eval_agent_ids=None):
+        self._update_world_view_range(world, eval_agent_ids)
         self.drawMap(world.map.get_open_drive_map())
 
         # draw agents
