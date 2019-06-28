aad5a4a8b9f9eaaa50a6e8b179b1ccc7bbaa391a
Patrick Hart <patrickhart.1990@gmail.com>
2019-06-28 09:13:01 +0200

    clean up code

--- modules/runtime/ml/tests/py_runtime_tfa_tests.py
+++ modules/runtime/ml/tests/py_runtime_tfa_tests.py
@@ -5,7 +5,6 @@
 
 
 import unittest
-import os
 from modules.runtime.scenario.scenario_generation.uniform_vehicle_distribution import UniformVehicleDistribution
 from modules.runtime.ml.runtime_rl import RuntimeRL
 from modules.runtime.ml.tfa_wrapper import TFAWrapper
@@ -14,29 +13,29 @@ from modules.runtime.ml.action_wrapper import MotionPrimitives
 from modules.runtime.ml.state_evaluator import GoalReached
 from modules.runtime.commons.parameters import ParameterServer
 from modules.runtime.viewer.matplotlib_viewer import MPViewer
-import numpy as np
 
 # tfa
 from tf_agents.environments import tf_py_environment
 from tf_agents.environments import utils
 
 class RuntimeRLTests(unittest.TestCase):
-    def test_motion_primitives_concat_state(self):
+    @staticmethod
+    def test_motion_primitives_concat_state():
         params = ParameterServer(filename="modules/runtime/tests/data/highway_merging.json")
         scenario_generation = UniformVehicleDistribution(num_scenarios=3, random_seed=0, params=params)
         state_observer = StateConcatenation(params=params)
         action_wrapper = MotionPrimitives(params=params)
         evaluator = GoalReached(params=params)
-        viewer = MPViewer(params=params, x_range=[-30,30], y_range=[-20,40], follow_agent_id=True) #use_world_bounds=True) # 
+        viewer = MPViewer(params=params, x_range=[-30,30], y_range=[-20,40], follow_agent_id=True) #use_world_bounds=True) #
 
         runtimerl = RuntimeRL(action_wrapper=action_wrapper, nn_observer=state_observer,
                               evaluator=evaluator, step_time=0.05, viewer=viewer,
                               scenario_generator=scenario_generation)
 
         tfa_env = TFAWrapper(runtimerl)
-        time_step = tfa_env.reset()
+        _ = tfa_env.reset()
         utils.validate_py_environment(tfa_env, episodes=5)
-        train_env = tf_py_environment.TFPyEnvironment(tfa_env)
+        _ = tf_py_environment.TFPyEnvironment(tfa_env)
 
 if __name__ == '__main__':
     unittest.main()
