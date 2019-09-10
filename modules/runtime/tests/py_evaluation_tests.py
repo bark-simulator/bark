8c33b7ffa55c378a64c735f2fb4d7db9de9f22a6
Patrick Hart <patrickhart.1990@gmail.com>
2019-09-10 11:26:51 +0200

    change

--- modules/runtime/tests/py_evaluation_tests.py
+++ modules/runtime/tests/py_evaluation_tests.py
@@ -21,40 +21,44 @@ from modules.runtime.commons.parameters import ParameterServer
 
 
 class EvaluationTests(unittest.TestCase):
-    def test_one_agent_at_goal(self):
-        param_server = ParameterServer()
-        # Model Definition
-        behavior_model = BehaviorConstantVelocity(param_server)
-        execution_model = ExecutionModelInterpolate(param_server)
-        dynamic_model = SingleTrackModel()
+  def test_one_agent_at_goal(self):
+    param_server = ParameterServer()
+    # Model Definition
+    behavior_model = BehaviorConstantVelocity(param_server)
+    execution_model = ExecutionModelInterpolate(param_server)
+    dynamic_model = SingleTrackModel()
 
-        print("test")
-        # Agent Definition
-        agent_2d_shape = CarLimousine()
-        init_state = np.array([0, -191.789,-50.1725, 3.14*3.0/4.0, 150/3.6])
-        agent_params = param_server.addChild("agent1")
-        goal_polygon = Polygon2d([0, 0, 0],[Point2d(-1,-1),Point2d(-1,1),Point2d(1,1), Point2d(1,-1)])
-        goal_polygon = goal_polygon.translate(Point2d(-191.789,-50.1725))
+    print("test")
+    # Agent Definition
+    agent_2d_shape = CarLimousine()
+    init_state = np.array([0, -191.789,-50.1725, 3.14*3.0/4.0, 150/3.6])
+    agent_params = param_server.addChild("agent1")
+    goal_polygon = Polygon2d([0, 0, 0],
+                             [Point2d(-1,-1),
+                              Point2d(-1,1),
+                              Point2d(1,1),
+                              Point2d(1,-1)])
+    goal_polygon = goal_polygon.translate(Point2d(-191.789,-50.1725))
 
-        agent = Agent(init_state,
-                    behavior_model,
-                    dynamic_model,
-                    execution_model,
-                    agent_2d_shape,
-                    agent_params,
-                    GoalDefinitionPolygon(goal_polygon),
-                     None)
+    agent = Agent(init_state,
+                behavior_model,
+                dynamic_model,
+                execution_model,
+                agent_2d_shape,
+                agent_params,
+                GoalDefinitionPolygon(goal_polygon),
+                  None)
 
-        world = World(param_server)
-        world.add_agent(agent)
-        evaluator = EvaluatorGoalReached(agent.id)
-        world.add_evaluator("success", evaluator)
+    world = World(param_server)
+    world.add_agent(agent)
+    evaluator = EvaluatorGoalReached(agent.id)
+    world.add_evaluator("success", evaluator)
 
 
-        info = world.evaluate()
-        self.assertEqual(info["success"], True)
-        
+    info = world.evaluate()
+    self.assertEqual(info["success"], True)
+      
         
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
 
