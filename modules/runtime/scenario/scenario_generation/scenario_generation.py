030e2ccbedaa9fdf7876f8677930ef17fc9748a8
Patrick Hart <patrickhart.1990@gmail.com>
2019-09-10 16:59:55 +0200

    .

--- modules/runtime/scenario/scenario_generation/scenario_generation.py
+++ modules/runtime/scenario/scenario_generation/scenario_generation.py
@@ -8,17 +8,17 @@ from modules.runtime.commons.parameters import ParameterServer
 
 class ScenarioGeneration:
     def __init__(self, params=None, num_scenarios=None, random_seed=None):
-        self.params = params
-        self.random_seed = random_seed
-        self.current_scenario_idx = 0
+        self._params = params
+        self._random_seed = random_seed
+        self._current_scenario_idx = 0
 
         if params is None:
-            self.params = ParameterServer()
+            self._params = ParameterServer()
         else:
-            self.params = params
-        self.initialize_params(params=self.params)
+            self._params = params
+        self.initialize_params(params=self._params)
 
-        self.scenario_list = self.create_scenarios(params, num_scenarios, random_seed)
+        self._scenario_list = self.create_scenarios(params, num_scenarios, random_seed)
 
     def initialize_params(self, params):
         """Initialize params or default params necessary for scenario creation
@@ -31,16 +31,16 @@ class ScenarioGeneration:
         pass
 
     def get_next_scenario(self):
-        if self.current_scenario_idx >= self.num_scenarios:
-            self.current_scenario_idx = 0
+        if self._current_scenario_idx >= self.num_scenarios:
+            self._current_scenario_idx = 0
             print("Resetting scenario index to zero")
-        scenario = self.get_scenario(self.current_scenario_idx)
-        scenario_idx = self.current_scenario_idx
-        self.current_scenario_idx += 1
+        scenario = self.get_scenario(self._current_scenario_idx)
+        scenario_idx = self._current_scenario_idx
+        self._current_scenario_idx += 1
         return scenario, scenario_idx
 
     def get_scenario(self, idx):
-        return self.scenario_list[idx].copy()
+        return self._scenario_list[idx].copy()
 
     def create_scenarios(self, params, num_scenarios, random_seed):
         """ Creates a list of scenario class instances which should be deterministically reproducible given the random seed, the params and the number of scenarios
@@ -57,13 +57,13 @@ class ScenarioGeneration:
 
     @property
     def num_scenarios(self):
-        return len(self.scenario_list)
+        return len(self._scenario_list)
 
 
     def dump_scenario_list(self, filename):
         with open(filename, "wb") as file:
-            pickle.dump(self.scenario_list, file)
+            pickle.dump(self._scenario_list, file)
 
     def load_scenario_list(self, filename):
         with open(filename, "rb") as file:
-            self.scenario_list = pickle.load(file)
+            self._scenario_list = pickle.load(file)
