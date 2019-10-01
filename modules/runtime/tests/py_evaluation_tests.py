3ea748df24a5f97543db5fe39cf84b813acd5c79
jbernhard <bernhard@fortiss.org>
2019-10-01 13:07:18 +0200

    added goal reached test

--- modules/runtime/tests/py_evaluation_tests.py
+++ modules/runtime/tests/py_evaluation_tests.py
@@ -92,6 +92,42 @@ class EvaluationTests(unittest.TestCase):
 
     info = world.evaluate()
     self.assertEqual(info["success"], True)
+
+  def test_one_agent_at_goal_state_limits(self):
+    param_server = ParameterServer()
+    # Model Definition
+    behavior_model = BehaviorConstantVelocity(param_server)
+    execution_model = ExecutionModelInterpolate(param_server)
+    dynamic_model = SingleTrackModel(param_server)
+
+    # Agent Definition
+    agent_2d_shape = CarLimousine()
+    init_state = np.array([0, -191.789,-50.1725, 3.14*3.0/4.0, 150/3.6])
+    agent_params = param_server.addChild("agent1")
+    goal_polygon = Polygon2d([0, 0, 0],
+                             [Point2d(-1,-1),
+                              Point2d(-1,1),
+                              Point2d(1,1),
+                              Point2d(1,-1)])
+    goal_polygon = goal_polygon.translate(Point2d(-191.789,-50.1725))
+
+    agent = Agent(init_state,
+                behavior_model,
+                dynamic_model,
+                execution_model,
+                agent_2d_shape,
+                agent_params,
+                GoalDefinitionStateLimits(goal_polygon, (3.14*3.0/4.0-0.08, 3.14*3.0/4.0+0.08)),
+                  None)
+
+    world = World(param_server)
+    world.add_agent(agent)
+    evaluator = EvaluatorGoalReached(agent.id)
+    world.add_evaluator("success", evaluator)
+
+
+    info = world.evaluate()
+    self.assertEqual(info["success"], True)
       
         
 if __name__ == '__main__':
