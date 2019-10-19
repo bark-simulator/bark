1691a4a87278fa0c435f53463a2f0ad9952e1c05
Andreas Kreutz <andreas.kreutz@hotmail.de>
2019-10-19 09:37:48 +0200

    Change function of the BehaviorPurePursuit model
    Pure pursuit no longer is a complete behavior model, but instead is only
    a means to obtain the steering angle based on a followed line.

--- modules/models/behavior/pure_pursuit/pure_pursuit.cpp
+++ modules/models/behavior/pure_pursuit/pure_pursuit.cpp
@@ -7,44 +7,44 @@ namespace modules {
 namespace models {
 namespace behavior {
 
-Trajectory BehaviorPurePursuit::Plan(float delta_time, const ObservedWorld &observed_world) {
-  using namespace dynamic;
+// Trajectory BehaviorPurePursuit::Plan(float delta_time, const ObservedWorld &observed_world) {
+//   using namespace dynamic;
 
-  //! TODO(@fortiss): parameters
-  const int num_traj_time_points = 100;
-  dynamic::Trajectory traj(num_traj_time_points, int(StateDefinition::MIN_STATE_SIZE));
-  auto const sample_time = delta_time / num_traj_time_points;
+//   //! TODO(@fortiss): parameters
+//   const int num_traj_time_points = 100;
+//   dynamic::Trajectory traj(num_traj_time_points, int(StateDefinition::MIN_STATE_SIZE));
+//   auto const sample_time = delta_time / num_traj_time_points;
 
-  dynamic::State ego_vehicle_state = observed_world.current_ego_state();
-  traj.row(0) = ego_vehicle_state;
+//   dynamic::State ego_vehicle_state = observed_world.current_ego_state();
+//   traj.row(0) = ego_vehicle_state;
   
-  double start_time = observed_world.get_world_time();
-  geometry::Line line;
-  if (!followed_line_.obj_.empty()) {
-    line = followed_line_;
-  } else {
-    line = observed_world.get_local_map()->get_driving_corridor().get_center();
-  }
-  if (line.obj_.size() > 0) {
-    for (int i = 1; i < num_traj_time_points; ++i) {
-      Input input(2);
-      input << 0.0f, FindSteeringAngle(traj.row(i - 1), line);
+//   double start_time = observed_world.get_world_time();
+//   geometry::Line line;
+//   if (!followed_line_.obj_.empty()) {
+//     line = followed_line_;
+//   } else {
+//     line = observed_world.get_local_map()->get_driving_corridor().get_center();
+//   }
+//   if (line.obj_.size() > 0) {
+//     for (int i = 1; i < num_traj_time_points; ++i) {
+//       Input input(2);
+//       input << 0.0f, FindSteeringAngle(traj.row(i - 1), line);
 
-      auto next_state = dynamic::euler_int(single_track_model_, traj.row(i - 1), input, sample_time);
+//       auto next_state = dynamic::euler_int(single_track_model_, traj.row(i - 1), input, sample_time);
 
-      traj(i, StateDefinition::TIME_POSITION) = start_time + i*sample_time;
-      traj(i, StateDefinition::X_POSITION) = next_state(StateDefinition::X_POSITION);
-      traj(i, StateDefinition::Y_POSITION) = next_state(StateDefinition::Y_POSITION);
-      traj(i, StateDefinition::THETA_POSITION) = next_state(StateDefinition::THETA_POSITION);
-      traj(i, StateDefinition::VEL_POSITION) = next_state(StateDefinition::VEL_POSITION);
-    }
-  }
+//       traj(i, StateDefinition::TIME_POSITION) = start_time + i*sample_time;
+//       traj(i, StateDefinition::X_POSITION) = next_state(StateDefinition::X_POSITION);
+//       traj(i, StateDefinition::Y_POSITION) = next_state(StateDefinition::Y_POSITION);
+//       traj(i, StateDefinition::THETA_POSITION) = next_state(StateDefinition::THETA_POSITION);
+//       traj(i, StateDefinition::VEL_POSITION) = next_state(StateDefinition::VEL_POSITION);
+//     }
+//   }
 
-  this->set_last_trajectory(traj);
-  return traj;
-}
+//   this->set_last_trajectory(traj);
+//   return traj;
+// }
 
-float BehaviorPurePursuit::FindSteeringAngle(const dynamic::State &agent_state, const geometry::Line &line) const {
+double BehaviorPurePursuit::FindSteeringAngle(const dynamic::State &agent_state) const {
   using namespace geometry;
   using namespace dynamic;
 
@@ -66,19 +66,18 @@ float BehaviorPurePursuit::FindSteeringAngle(const dynamic::State &agent_state, 
   //   }
   // }
 
-  // TODO(@AKreutz): get gain and wheelbase from params
-  float gain = 1.0f;
-  float Lf = 2.6f;
+  // TODO(@AKreutz): get gain from params
+  double gain = 1.0f;
 
   // TODO(@AKreutz): probably only works for straight lines
   Point2d agent_position(agent_state(StateDefinition::X_POSITION), agent_state(StateDefinition::Y_POSITION));
-  float lookahead_distance = gain * agent_state(StateDefinition::VEL_POSITION);
-  float nearest_s = get_nearest_s(line, agent_position);
-  Point2d lookahead_point = get_point_at_s(line, nearest_s + lookahead_distance);
+  double lookahead_distance = gain * agent_state(StateDefinition::VEL_POSITION);
+  double nearest_s = get_nearest_s(followed_line_, agent_position);
+  Point2d lookahead_point = get_point_at_s(followed_line_, nearest_s + lookahead_distance);
 
-  float lookahead_angle = atan2(bg::get<1>(lookahead_point - agent_position), bg::get<0>(lookahead_point - agent_position)) - agent_state(StateDefinition::THETA_POSITION);
+  double lookahead_angle = atan2(bg::get<1>(lookahead_point - agent_position), bg::get<0>(lookahead_point - agent_position)) - agent_state(StateDefinition::THETA_POSITION);
   
-  return atan(2*Lf*sin(lookahead_angle) / distance(agent_position, lookahead_point));
+  return atan(2*wheel_base_*sin(lookahead_angle) / distance(agent_position, lookahead_point));
 }
 
 } // namespace behavior
--- modules/models/behavior/pure_pursuit/pure_pursuit.hpp
+++ modules/models/behavior/pure_pursuit/pure_pursuit.hpp
@@ -13,30 +13,24 @@ namespace behavior {
 using world::ObservedWorld;
 
 // From technical report "Implementation of the Pure Pursuit Path Tracking Algorithm"
-class BehaviorPurePursuit : public BehaviorModel {
+class BehaviorPurePursuit {
   public:
     explicit BehaviorPurePursuit(commons::Params *params) :
-      BehaviorModel(params), single_track_model_(params) {}
+        single_track_model_(params) {
+      wheel_base_ = params->get_real("DynamicModel::wheel_base", "Wheel base of vehicle.", 2.7);
+    }
     
-    virtual ~BehaviorPurePursuit() {}
-
-    Trajectory Plan(float delta_time,
-                    const ObservedWorld& observed_world);
+    double FindSteeringAngle(const dynamic::State &agent_state) const;
 
     void set_followed_line(const geometry::Line followed_line) { followed_line_ = followed_line; }
+    bool is_following_line() const { return followed_line_.size() != 0; }
 
-    virtual BehaviorModel *Clone() const;
-  
   private:
-    float FindSteeringAngle(const dynamic::State &agent_state, const geometry::Line &line) const;
-
     dynamic::SingleTrackModel single_track_model_;
     geometry::Line followed_line_;
-};
 
-inline BehaviorModel *BehaviorPurePursuit::Clone() const {
-  return new BehaviorPurePursuit(*this);
-}
+    float wheel_base_;
+};
 
 } // namespace behavior
 } // namespace models
