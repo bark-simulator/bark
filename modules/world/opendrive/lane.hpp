db33afdfdbee03d20c0f9cac5299537e1ee2e77e
Klemens Esterle <esterle@fortiss.org>
2020-01-02 16:31:00 +0100

    formatting

--- modules/world/opendrive/lane.hpp
+++ modules/world/opendrive/lane.hpp
@@ -1,15 +1,15 @@
-// Copyright (c) 2019 fortiss GmbH, Julian Bernhard, Klemens Esterle, Patrick Hart, Tobias Kessler
+// Copyright (c) 2019 fortiss GmbH, Julian Bernhard, Klemens Esterle, Patrick
+// Hart, Tobias Kessler
 //
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
-
 #ifndef MODULES_WORLD_OPENDRIVE_LANE_HPP_
 #define MODULES_WORLD_OPENDRIVE_LANE_HPP_
 
+#include <map>
 #include <string>
 #include <vector>
-#include <map>
 #include "modules/geometry/commons.hpp"
 #include "modules/geometry/line.hpp"
 #include "modules/world/opendrive/commons.hpp"
@@ -18,13 +18,24 @@ namespace modules {
 namespace world {
 namespace opendrive {
 
-
 class Lane {
  public:
-  Lane() : lane_id_(++lane_count), lane_position_(0),
-           link_(), line_(), lane_type_(LaneType::NONE), road_mark_(), speed_() {}
-  explicit Lane(const LanePosition& lane_position) : lane_id_(++lane_count),
-   lane_position_(lane_position), link_(), line_(), lane_type_(LaneType::NONE), road_mark_(), speed_() {}
+  Lane()
+      : lane_id_(++lane_count),
+        lane_position_(0),
+        link_(),
+        line_(),
+        lane_type_(LaneType::NONE),
+        road_mark_(),
+        speed_() {}
+  explicit Lane(const LanePosition& lane_position)
+      : lane_id_(++lane_count),
+        lane_position_(lane_position),
+        link_(),
+        line_(),
+        lane_type_(LaneType::NONE),
+        road_mark_(),
+        speed_() {}
 
   ~Lane() {}
 
@@ -35,10 +46,12 @@ class Lane {
   void set_speed(float speed) { speed_ = speed; }
   void set_lane_type(const LaneType lt) { lane_type_ = lt; }
   void set_road_mark(const RoadMark rm) { road_mark_ = rm; }
-  void set_lane_position(const LanePosition& lane_position)
-                             { lane_position_ = lane_position; }
+  void set_lane_position(const LanePosition& lane_position) {
+    lane_position_ = lane_position;
+  }
 
-  bool append(geometry::Line previous_line, LaneWidth lane_width_current, float s_inc);
+  bool append(geometry::Line previous_line, LaneWidth lane_width_current,
+              float s_inc);
 
   //! getter functions
   geometry::Line get_line() const { return line_; }
@@ -46,15 +59,14 @@ class Lane {
   LaneLink get_link() const { return link_; }
   RoadMark get_road_mark() const { return road_mark_; }
   float get_speed() const { return speed_; }
-  LaneType get_lane_type() const { return lane_type_;}
+  LaneType get_lane_type() const { return lane_type_; }
   LaneId get_id() const { return lane_id_; }
-  LanePosition get_lane_position() const { return lane_position_;}
+  LanePosition get_lane_position() const { return lane_position_; }
 
   float curvature_at(const float s, const float s_delta = 0.01) const;
   float curvature_dot_at(const float s) const;
   float lane_width_at(const float s) const;
-  float s_from_point(const geometry::Point2d &point) const;
-
+  float s_from_point(const geometry::Point2d& point) const;
 
  private:
   LaneId lane_id_;
@@ -69,7 +81,7 @@ class Lane {
   static LaneId lane_count;
 };
 
-inline std::string print(const Lane &l) {
+inline std::string print(const Lane& l) {
   std::stringstream ss;
   ss << "id: " << l.get_id() << ", ";
   ss << "position " << l.get_lane_position() << ", ";
@@ -84,16 +96,16 @@ using LaneSequence = std::vector<LaneId>;
 using LaneSequences = std::vector<LaneSequence>;
 using Lanes = std::map<LaneId, LanePtr>;
 
-
-inline LanePtr create_lane_from_lane_width(LanePosition lane_position, geometry::Line previous_line, LaneWidth lane_width_current, float s_inc = 0.5f) {
-  
+inline LanePtr create_lane_from_lane_width(LanePosition lane_position,
+                                           geometry::Line previous_line,
+                                           LaneWidth lane_width_current,
+                                           float s_inc = 0.5f) {
   std::shared_ptr<Lane> ret_lane(new Lane(lane_position));
 
   bool succ = ret_lane->append(previous_line, lane_width_current, s_inc);
 
   return ret_lane;
 }
-
 
 }  // namespace opendrive
 }  // namespace world
