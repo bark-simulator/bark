6db9722a20685020254f67d4959507c4295bc3a9
Patrick Hart <patrickhart.1990@gmail.com>
2020-01-08 17:09:04 +0100

    Removed log messages

--- examples/analyze_map.py
+++ examples/analyze_map.py
@@ -19,7 +19,7 @@ import numpy as np
 
 # Name and Output Directory
 # CHANGE THIS #
-map_name = "CulDeSac"
+map_name = "4way_intersection"
 output_dir = "/home/hart/Dokumente/2020/" + map_name
 
 # Map Definition
@@ -46,48 +46,44 @@ viewer.saveFig(output_dir + "/" + "world_plain.png")
 color_triplet_gray = (0.7,0.7,0.7)
 
 # Open Drive Elements (Roads, Lane Sections, Lanes)
-# for idx_r, road in open_drive_map.get_roads().items():
-#   viewer.drawWorld(world)
-#   viewer.drawRoad(road)
-#   viewer.saveFig(output_dir + "/" + "open_drive_map_road_" + str(idx_r) + ".png")
-#   viewer.show()
-#   viewer.clear()
+for idx_r, road in open_drive_map.get_roads().items():
+  viewer.drawWorld(world)
+  viewer.drawRoad(road)
+  viewer.saveFig(output_dir + "/" + "open_drive_map_road_" + str(idx_r) + ".png")
+  viewer.show()
+  viewer.clear()
 
-# for idx_r, road in open_drive_map.get_roads().items():
-#   for idx_ls, lane_section in enumerate(road.lane_sections):
-#     viewer.drawWorld(world)
-#     viewer.drawRoad(road, color_triplet_gray)
-#     viewer.drawLaneSection(lane_section)
-#     viewer.saveFig(output_dir + "/" + "open_drive_map_road_" + str(idx_r) + "_lane_section" + str(idx_ls) + ".png")
-#     viewer.show()
-#     viewer.clear()
+for idx_r, road in open_drive_map.get_roads().items():
+  for idx_ls, lane_section in enumerate(road.lane_sections):
+    viewer.drawWorld(world)
+    viewer.drawRoad(road, color_triplet_gray)
+    viewer.drawLaneSection(lane_section)
+    viewer.saveFig(output_dir + "/" + "open_drive_map_road_" + str(idx_r) + "_lane_section" + str(idx_ls) + ".png")
+    viewer.show()
+    viewer.clear()
 
-# for idx_r, road in open_drive_map.get_roads().items():
-#   for idx_ls, lane_section in enumerate(road.lane_sections):
-#     for idx_l, lane in lane_section.get_lanes().items():
-#       viewer.drawWorld(world)
-#       viewer.drawRoad(road, color_triplet_gray)
-#       viewer.drawLaneSection(lane_section, color_triplet_gray)
-#       viewer.drawLane(lane)
-#       viewer.saveFig(output_dir + "/" + "open_drive_map_road_" + str(idx_r) + "_lane_section" + str(idx_ls) + "_lane" + str(idx_l) + ".png")
-#       viewer.show()
-#       viewer.clear()
+for idx_r, road in open_drive_map.get_roads().items():
+  for idx_ls, lane_section in enumerate(road.lane_sections):
+    for idx_l, lane in lane_section.get_lanes().items():
+      viewer.drawWorld(world)
+      viewer.drawRoad(road, color_triplet_gray)
+      viewer.drawLaneSection(lane_section, color_triplet_gray)
+      viewer.drawLane(lane)
+      viewer.saveFig(output_dir + "/" + "open_drive_map_road_" + str(idx_r) + "_lane_section" + str(idx_ls) + "_lane" + str(idx_l) + ".png")
+      viewer.show()
+      viewer.clear()
 
 # Lanes of Roadgraph
 roadgraph = map_interface.get_roadgraph()
 roadgraph.print_graph(output_dir + "/" + map_name)
 lane_ids = roadgraph.get_all_laneids ()
 
-# for lane_id in lane_ids:
-for lane_id in [244]:
+for lane_id in lane_ids:
   lane_polygon = roadgraph.get_lane_polygon_by_id(lane_id)
-
   # plot plan_view
   road_id = roadgraph.get_road_by_lane_id(lane_id)
-  print(road_id)
   road = map_interface.get_open_drive_map().get_road(road_id)
   plan_view_reference = road.plan_view.get_reference_line()
-
   # plot polygon with center line
   outer, inner = roadgraph.compute_lane_boundaries(lane_id)
   center_line = compute_center_line(outer.line, inner.line)
@@ -97,7 +93,7 @@ for lane_id in [244]:
   viewer.drawLine2d(center_line)
   viewer.drawLine2d(plan_view_reference, color="red")
   viewer.saveFig(output_dir + "/" + "roadgraph_laneid_" + str(lane_id) + ".png")
-  viewer.show(block=True)
+  viewer.show(block=False)
 
 
 #for rc in all_corridors:
