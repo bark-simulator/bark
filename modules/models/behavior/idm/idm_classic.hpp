0ab0eca6e62d87a80c3178c9f26f1f91c8212bee
Klemens Esterle <klemens.esterle@posteo.de>
2020-01-26 20:34:28 +0100

    modified idm interface to use it for two arbitrary agents

--- modules/models/behavior/idm/idm_classic.hpp
+++ modules/models/behavior/idm/idm_classic.hpp
@@ -1,15 +1,15 @@
-// Copyright (c) 2019 fortiss GmbH, Julian Bernhard, Klemens Esterle, Patrick Hart, Tobias Kessler
+// Copyright (c) 2019 fortiss GmbH, Julian Bernhard, Klemens Esterle, Patrick
+// Hart, Tobias Kessler
 //
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
-
 
 #ifndef MODULES_MODELS_BEHAVIOR_IDM_IDM_CLASSIC_HPP_
 #define MODULES_MODELS_BEHAVIOR_IDM_IDM_CLASSIC_HPP_
 
 #include "modules/models/behavior/longitudinal_acceleration/longitudinal_acceleration.hpp"
-#include "modules/world/world.hpp"
 #include "modules/world/map/frenet.hpp"
+#include "modules/world/world.hpp"
 
 namespace modules {
 namespace models {
@@ -17,30 +17,33 @@ namespace behavior {
 
 class BehaviorIDMClassic : public BehaviorLongitudinalAcceleration {
  public:
-  explicit BehaviorIDMClassic(commons::Params *params) :
-    BehaviorLongitudinalAcceleration(params) {}
+  explicit BehaviorIDMClassic(commons::Params* params)
+      : BehaviorLongitudinalAcceleration(params) {}
 
   virtual ~BehaviorIDMClassic() {}
 
+  double CalculateLongitudinalAccelerationTwoAgents(
+      const std::shared_ptr<const world::objects::Agent>& ego_agent,
+      const std::shared_ptr<const world::objects::Agent>& leading_agent, const double distance);
+
   virtual double CalculateLongitudinalAcceleration(
-    const world::ObservedWorld& observed_world);
+      const world::ObservedWorld& observed_world);
 
-  std::pair<world::objects::AgentPtr, modules::world::map::Frenet>
-  GetLeadingVehicle(const world::ObservedWorld& observed_world);
-
-  const double get_desired_velocity() {return 15.0f;}  // unit is meter/second
-  const float get_minimum_spacing() {return 2.0f;}  // unit is meter
-  const float get_desired_time_headway() {return 1.5f;}  // unit is seconds
-  const float get_max_acceleration() {return 1.7f;}  // unit is meter/second^2
-  const float get_comfortable_braking_acceleration() {return 1.67f;}  // unit is meter/second^2
-  const int get_exponent() { return  4;}
+  const double get_desired_velocity() { return 15.0f; }  // unit is meter/second
+  const float get_minimum_spacing() { return 2.0f; }     // unit is meter
+  const float get_desired_time_headway() { return 1.5f; }  // unit is seconds
+  const float get_max_acceleration() { return 1.7f; }  // unit is meter/second^2
+  const float get_comfortable_braking_acceleration() {
+    return 1.67f;
+  }  // unit is meter/second^2
+  const int get_exponent() { return 4; }
 
   virtual std::shared_ptr<BehaviorModel> Clone() const;
 };
 
 inline std::shared_ptr<BehaviorModel> BehaviorIDMClassic::Clone() const {
   std::shared_ptr<BehaviorIDMClassic> model_ptr =
-    std::make_shared<BehaviorIDMClassic>(*this);
+      std::make_shared<BehaviorIDMClassic>(*this);
   return std::dynamic_pointer_cast<BehaviorModel>(model_ptr);
 }
 
