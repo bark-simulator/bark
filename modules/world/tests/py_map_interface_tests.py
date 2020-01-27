f885475b476181ed12857d8f740cfeb0b8e4b653
Patrick Hart <patrickhart.1990@gmail.com>
2020-01-27 17:19:55 +0100

    Scenario generation still needs to be fixed

--- modules/world/tests/py_map_interface_tests.py
+++ modules/world/tests/py_map_interface_tests.py
@@ -75,18 +75,18 @@ class EnvironmentTests(unittest.TestCase):
     world.SetMap(map_interface)
 
     # Simple test
-    point_close = Point2d(5112.68262, 5086.44971)
+    point_close = Point2d(5114.68262, 5086.44971)
     lane_sw = map_interface.FindLane(point_close)
-    self.assertIsNotNone(lane_sw, "This point is still in the left lane! XodrLane boundary is 5112.683")
+    self.assertIsNotNone(lane_sw, "This point is still in the left lane! XodrLane boundary is 5114.683")
 
     switched_lane = False
     lng_coord = 5086.44971
-    i = 5112.0
+    i = 5114.5
     lane_sw = map_interface.FindLane(Point2d(i, lng_coord))
     assert lane_sw != None
     prev = lane_sw.lane_id
     prev_i = i
-    while (i < 5113.0):
+    while (i < 5117.5):
       lane_sw = map_interface.FindLane(Point2d(i, lng_coord))
       self.assertIsNotNone(lane_sw, "Should always be on at least one lane! Currently at ({}, {})".format(i, lng_coord))
       if prev != lane_sw.lane_id:
@@ -98,7 +98,7 @@ class EnvironmentTests(unittest.TestCase):
         switched_lane = True
       prev_i = i
       prev = lane_sw.lane_id
-      i = i + 0.0001
+      i = i + 0.01
     self.assertTrue(switched_lane, "Eventually should have switched lanes!")
 
   def test_find_lane(self):
@@ -126,7 +126,7 @@ class EnvironmentTests(unittest.TestCase):
     map_interface = MapInterface()
     map_interface.SetOpenDriveMap(xodr_parser.map)
     world.SetMap(map_interface)
-    point = Point2d(5111, 5072)
+    point = Point2d(5114, 5072)
     viewer = MPViewer(params=params, use_world_bounds=True)
     viewer.drawWorld(world)
     viewer.drawPoint2d(point, 'red', 1.0)
