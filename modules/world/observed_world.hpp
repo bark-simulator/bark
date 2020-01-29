9317d6a3308685e433c2124b98a0466dc00a01ca
Klemens Esterle <esterle@fortiss.org>
2020-01-29 16:03:41 +0100

    implemented GetAgentInFrontForId

--- modules/world/observed_world.hpp
+++ modules/world/observed_world.hpp
@@ -1,8 +1,8 @@
-// Copyright (c) 2019 fortiss GmbH, Julian Bernhard, Klemens Esterle, Patrick Hart, Tobias Kessler
+// Copyright (c) 2019 fortiss GmbH, Julian Bernhard, Klemens Esterle, Patrick
+// Hart, Tobias Kessler
 //
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
-
 
 #ifndef MODULES_WORLD_OBSERVED_WORLD_HPP_
 #define MODULES_WORLD_OBSERVED_WORLD_HPP_
@@ -10,97 +10,98 @@
 #include <unordered_map>
 #include <utility>
 #include "modules/geometry/geometry.hpp"
-#include "modules/world/world.hpp"
+#include "modules/models/dynamic/dynamic_model.hpp"
 #include "modules/world/map/frenet.hpp"
 #include "modules/world/prediction/prediction_settings.hpp"
-#include "modules/models/dynamic/dynamic_model.hpp"
+#include "modules/world/world.hpp"
 
 namespace modules {
 namespace world {
 
-using world::objects::AgentId;
-using world::objects::AgentPtr;
-using world::objects::Agent;
-using world::map::MapInterfacePtr;
-using world::map::RoadCorridorPtr;
 using modules::geometry::Point2d;
-using modules::models::dynamic::State;
-using modules::models::dynamic::StateDefinition::X_POSITION;
-using modules::models::dynamic::StateDefinition::Y_POSITION;
 using modules::models::behavior::BehaviorModel;
 using modules::models::behavior::BehaviorModelPtr;
 using modules::models::behavior::DiscreteAction;
+using modules::models::dynamic::State;
+using modules::models::dynamic::StateDefinition::X_POSITION;
+using modules::models::dynamic::StateDefinition::Y_POSITION;
 using modules::world::prediction::PredictionSettings;
-
+using world::map::MapInterfacePtr;
+using world::map::RoadCorridorPtr;
+using world::map::LaneCorridorPtr;
+using world::objects::Agent;
+using world::objects::AgentId;
+using world::objects::AgentPtr;
+using modules::world::map::Frenet;
 
 class ObservedWorld : public World {
  public:
-    ObservedWorld(const WorldPtr& world,
-                  const AgentId& ego_agent_id) :
-      World(world),
-      ego_agent_id_(ego_agent_id) {}
+  ObservedWorld(const WorldPtr& world, const AgentId& ego_agent_id)
+      : World(world), ego_agent_id_(ego_agent_id) {}
 
-    ~ObservedWorld() {}
+  ~ObservedWorld() {}
 
-    std::pair<AgentPtr, modules::world::map::Frenet> GetAgentInFront() const;
+  std::pair<AgentPtr, Frenet> GetAgentInFrontForId(
+      const AgentId& agent_id,
+      const LaneCorridorPtr& lane_corridor) const;
 
-    std::pair<AgentPtr, modules::world::map::Frenet> GetAgentBehind() const;
+  std::pair<AgentPtr, modules::world::map::Frenet> GetAgentInFront() const;
 
-    virtual double GetWorldTime() const { return World::GetWorldTime(); }
+  std::pair<AgentPtr, modules::world::map::Frenet> GetAgentBehind() const;
 
-    const RoadCorridorPtr GetRoadCorridor() const {
-      return ObservedWorld::GetEgoAgent()->GetRoadCorridor();
-    }
+  virtual double GetWorldTime() const { return World::GetWorldTime(); }
 
-    std::shared_ptr<const Agent> GetEgoAgent() const {
-      return World::GetAgent(ego_agent_id_);
-    }
+  const RoadCorridorPtr GetRoadCorridor() const {
+    return ObservedWorld::GetEgoAgent()->GetRoadCorridor();
+  }
 
-    AgentMap GetOtherAgents() const {
-      auto tmp_map = World::GetAgents();
-      tmp_map.erase(ego_agent_id_);
-      return tmp_map;
-    }
+  std::shared_ptr<const Agent> GetEgoAgent() const {
+    return World::GetAgent(ego_agent_id_);
+  }
 
-    const std::shared_ptr<BehaviorModel> GetEgoBehaviorModel() const {
-      return World::GetAgents()[ego_agent_id_]->GetBehaviorModel();
-    }
+  AgentMap GetOtherAgents() const {
+    auto tmp_map = World::GetAgents();
+    tmp_map.erase(ego_agent_id_);
+    return tmp_map;
+  }
 
-    void SetEgoBehaviorModel(const BehaviorModelPtr& behavior_model) const {
-      return World::GetAgents()[ego_agent_id_]->SetBehaviorModel(
-        behavior_model);
-    }
+  const std::shared_ptr<BehaviorModel> GetEgoBehaviorModel() const {
+    return World::GetAgents()[ego_agent_id_]->GetBehaviorModel();
+  }
 
-    void SetBehaviorModel(const AgentId& agent_id,
-                            const BehaviorModelPtr& behavior_model) const {
-      return World::GetAgents()[agent_id]->SetBehaviorModel(behavior_model);
-    }
+  void SetEgoBehaviorModel(const BehaviorModelPtr& behavior_model) const {
+    return World::GetAgents()[ego_agent_id_]->SetBehaviorModel(behavior_model);
+  }
 
-    const MapInterfacePtr GetMap() const { return World::GetMap(); }
+  void SetBehaviorModel(const AgentId& agent_id,
+                        const BehaviorModelPtr& behavior_model) const {
+    return World::GetAgents()[agent_id]->SetBehaviorModel(behavior_model);
+  }
 
-    virtual State CurrentEgoState() const {
-      return World::GetAgents()[ego_agent_id_]->GetCurrentState();
-    }
+  const MapInterfacePtr GetMap() const { return World::GetMap(); }
 
-    Point2d CurrentEgoPosition() const {
-      return World::GetAgents()[ego_agent_id_]->GetCurrentPosition();
-    }
+  virtual State CurrentEgoState() const {
+    return World::GetAgents()[ego_agent_id_]->GetCurrentState();
+  }
 
-    void SetupPrediction(const PredictionSettings& settings);
+  Point2d CurrentEgoPosition() const {
+    return World::GetAgents()[ego_agent_id_]->GetCurrentPosition();
+  }
 
-    WorldPtr Predict(float time_span,
-                     const DiscreteAction& ego_action) const;
-    WorldPtr Predict(float time_span) const;
+  void SetupPrediction(const PredictionSettings& settings);
 
-    virtual WorldPtr Clone() const {
-      WorldPtr world_clone(World::Clone());
-      std::shared_ptr<ObservedWorld> observed_world =
+  WorldPtr Predict(float time_span, const DiscreteAction& ego_action) const;
+  WorldPtr Predict(float time_span) const;
+
+  virtual WorldPtr Clone() const {
+    WorldPtr world_clone(World::Clone());
+    std::shared_ptr<ObservedWorld> observed_world =
         std::make_shared<ObservedWorld>(world_clone, this->ego_agent_id_);
-      return std::dynamic_pointer_cast<World>(observed_world);
-    }
+    return std::dynamic_pointer_cast<World>(observed_world);
+  }
 
  private:
-    AgentId ego_agent_id_;
+  AgentId ego_agent_id_;
 };
 
 typedef std::shared_ptr<ObservedWorld> ObservedWorldPtr;
--- modules/world/tests/observed_world_test.cc
+++ modules/world/tests/observed_world_test.cc
@@ -32,6 +32,90 @@ using namespace modules::world;
 using namespace modules::world::evaluation;
 using namespace modules::world::prediction;
 using StateDefinition::VEL_POSITION;
+/*TEST(observed_world, agent_in_front) {
+  DefaultParams params;
+  ExecutionModelPtr exec_model(new ExecutionModelInterpolate(&params));
+  DynamicModelPtr dyn_model(new SingleTrackModel(&params));
+  BehaviorModelPtr beh_model(new BehaviorConstantVelocity(&params));
+  EvaluatorPtr col_checker(new EvaluatorCollisionAgents());
+
+  Polygon polygon(
+    Pose(1.25, 1, 0),
+    std::vector<Point2d>{Point2d(0, 0),
+                         Point2d(0, 2),
+                         Point2d(4, 2),
+                         Point2d(4, 0),
+                         Point2d(0, 0)});
+
+  State init_state1(
+    static_cast<int>(StateDefinition::MIN_STATE_SIZE));
+  init_state1 << 0.0, 0.0, 0.0, 0.0, 5.0;
+  AgentPtr agent1(new Agent(init_state1, beh_model, dyn_model, exec_model, polygon, &params));  // NOLINT
+
+  State init_state2(
+    static_cast<int>(StateDefinition::MIN_STATE_SIZE));
+  init_state2 << 0.0, 8.0, 0.0, 0.0, 5.0;
+  AgentPtr agent2(new Agent(init_state2, beh_model, dyn_model, exec_model, polygon, &params));  // NOLINT
+
+  WorldPtr world(new World(&params));
+  world->AddAgent(agent1);
+  world->AddAgent(agent2);
+  world->UpdateAgentRTree();
+
+
+  // Define some driving corridor and add to local map of agent
+  Line center;
+  center.AddPoint(Point2d(1, 1));
+  center.AddPoint(Point2d(2, 1));
+  center.AddPoint(Point2d(10, 1));
+
+  Line outer;
+  outer.AddPoint(Point2d(1, 2));
+  outer.AddPoint(Point2d(2, 2));
+  outer.AddPoint(Point2d(10, 2));
+
+  Line inner;
+  inner.AddPoint(Point2d(1, 0));
+  inner.AddPoint(Point2d(2, 0));
+  inner.AddPoint(Point2d(10, 0));
+
+  RoadCorridorPtr road_corridor(new modules::models::tests::DummyRoadCorridor(center, outer, inner));
+  agent1->SetRoadCorridor(road_corridor);
+  agent2->SetRoadCorridor(road_corridor);
+
+  WorldPtr current_world_state(world->Clone());
+  ObservedWorld observed_world(current_world_state, agent2->GetAgentId());
+
+  // Leading agent should not have an agent in front
+  std::pair<AgentPtr, Frenet> leading_vehicle =
+    observed_world.GetAgentInFront();
+  EXPECT_FALSE(static_cast<bool>(leading_vehicle.first));
+
+  WorldPtr current_world_state2(world->Clone());
+  ObservedWorld observed_world2(current_world_state2, agent1->GetAgentId());
+
+  // Agent behind should have leading agent in front
+  std::pair<AgentPtr, Frenet> leading_vehicle2 =
+    observed_world2.GetAgentInFront();
+  EXPECT_TRUE(static_cast<bool>(leading_vehicle2.first));
+  EXPECT_EQ(leading_vehicle2.first->GetAgentId(), agent2->GetAgentId());
+
+  State init_state3(static_cast<int>(StateDefinition::MIN_STATE_SIZE));
+  init_state3 << 0.0, 20.0, 0.0, 0.0, 5.0;
+  AgentPtr agent3(new Agent(init_state3, beh_model, dyn_model, exec_model, polygon, &params));  // NOLINT
+  world->AddAgent(agent3);
+  world->UpdateAgentRTree();
+
+  WorldPtr current_world_state3(world->Clone());
+  ObservedWorld observed_world3(current_world_state3, agent1->GetAgentId());
+
+  // Adding a third agent in front of leading agent, still leading agent should be in front
+  std::pair<AgentPtr, Frenet> leading_vehicle3 =
+    observed_world3.GetAgentInFront();
+  EXPECT_TRUE(static_cast<bool>(leading_vehicle3.first));
+  EXPECT_EQ(leading_vehicle2.first->GetAgentId(), agent2->GetAgentId());
+
+}*/
 
 TEST(observed_world, agent_in_front) {
   DefaultParams params;
