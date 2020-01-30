510595ca05c6a1c309ade2695be40f470060e2e9
jbernhard <bernhard@fortiss.org>
2020-01-30 16:14:23 +0100

    config reader implementations and interface changes

--- modules/runtime/scenario/scenario_generation/config_readers/config_readers_interfaces.py
+++ modules/runtime/scenario/scenario_generation/config_readers/config_readers_interfaces.py
@@ -1,51 +1,54 @@
+# Copyright (c) 2019 fortiss GmbH
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
 
-
-import abc
+from abs import ABC, abstractmethod
 
 # The property-based scenario generation must implement all of this interfaces. Property readers only the respective interface
-class PropertyReaderBehaviorModels:
+class ConfigReaderBehaviorModels(ABC):
   # returns list of size num agents with behavior models based on property, default_params_dict
   @abstractmethod
-  def create_from_properties(property_param_object, road_corridor, agent_states,  **kwargs):
+  def create_from_config(config_param_object, road_corridor, agent_states,  **kwargs):
     pass
 
-class PropertyReaderExecutionModels:
+class ConfigReaderExecutionModels(ABC):
   # returns list of size num agents with execution models based on property, default_params_dict
   @abstractmethod
-  def create_from_properties(property_param_object, road_corridor, agent_states,  **kwargs):
+  def create_from_config(config_param_object, road_corridor, agent_states,  **kwargs):
     pass
 
-class PropertyReaderDynamicModels:
+class ConfigReaderDynamicModels(ABC):
   # returns list of size num agents with dynamic models based on property, default_params_dict
   @abstractmethod
-  def create_from_properties(property_param_object, road_corridor, agent_states,  **kwargs):
+  def create_from_config(config_param_object, road_corridor, agent_states,  **kwargs):
     pass
 
-class PropertyReaderAgentStatesAndGeometries:
+class ConfigReaderAgentStatesAndGeometries(ABC):
   @abstractmethod
   # returns first a tuple containing 1] list of size num agents each element being a python list representing an agent state
   # and 2] a list of size num agents of point lists defining the agent geometries
   #  3] None or a dict passed as kwargs to the other functions (
   # e.g. the dict can contain a type entry specifying for each agent if it is a truck or car)
   # and second a default_params_dict
-  def create_from_properties(property_param_object, road_corridor):
+  def create_from_config(config_param_object, road_corridor):
     pass
 
-class PropertyReaderDynamicModels:
+class ConfigReaderDynamicModels(ABC):
   # returns list of size num agents with behavior models based on property, default_params_dict
   @abstractmethod
-  def create_from_properties(property_param_object, road_corridor, agent_states,  **kwargs):
+  def create_from_config(config_param_object, road_corridor, agent_states,  **kwargs):
     pass
 
 
-class PropertyReaderGoalDefinitions:
+class ConfigReaderGoalDefinitions(ABC):
   # returns list of size num agents with goal definitions for each agent based on property, default_params_dict
   @abstractmethod
-  def create_from_properties(property_param_object, road_corridor, agent_states,  **kwargs):
+  def create_from_config(config_param_object, road_corridor, agent_states,  **kwargs):
     pass
 
-class PropertyReaderGoalDefinitions:
+class ConfigReaderGoalDefinitions(ABC):
    # returns list of size num agents with true or false depending if agent is controlled or not for each agent based on property, default_params_dict
   @abstractmethod
-  def create_from_properties(property_param_object, road_corridor, agent_states,  **kwargs):
+  def create_from_config(config_param_object, road_corridor, agent_states,  **kwargs):
     pass
