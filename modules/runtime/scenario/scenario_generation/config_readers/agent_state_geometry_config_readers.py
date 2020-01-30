6811880ae4a3f17e55302dab8a42479dcdbdaa88
jbernhard <bernhard@fortiss.org>
2020-01-30 16:15:11 +0100

    clear agent state geometry inf

--- modules/runtime/scenario/scenario_generation/config_readers/agent_state_geometry_config_readers.py
+++ modules/runtime/scenario/scenario_generation/config_readers/agent_state_geometry_config_readers.py
@@ -6,25 +6,3 @@
 
 
 
-from config_readers_interfaces import ConfigReaderBehaviorModels
-
-  import ModelJsonConversion
-from bark.world.agent import *
-from bark.models.behavior import *
-from bark.world import *
-from bark.world.goal_definition import GoalDefinition, GoalDefinitionPolygon, GoalDefinitionStateLimits
-from bark.world.map import *
-from bark.models.dynamic import *
-from bark.models.execution import *
-from bark.geometry import *
-from bark.geometry.standard_shapes import *
-from modules.runtime.commons.parameters import ParameterServer
-from modules.runtime.commons.xodr_parser import XodrParser
-
-
-class UniformDistribution(ConfigReaderBehaviorModels):
-  def create_from_properties(property_param_object, road_corridor, agent_states,  **kwargs):
-    behavior_model = BehaviorConstantVelocity(param_server)
-
-    behavior_models = []
-    for _ in agent_states:
