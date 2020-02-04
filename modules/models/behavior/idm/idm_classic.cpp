1d18b633605ce1a8dfb623d1612028cd19c1bcf2
Klemens Esterle <esterle@fortiss.org>
2020-02-04 17:09:49 +0100

    fixed interaction term

--- modules/models/behavior/idm/idm_classic.cpp
+++ modules/models/behavior/idm/idm_classic.cpp
@@ -21,40 +21,58 @@ using modules::world::objects::Agent;
 using modules::geometry::Point2d;
 using modules::commons::transformation::FrenetPosition;
 
-double BehaviorIDMClassic::CalcLongAccTwoAgents(const std::shared_ptr<const Agent>& ego_agent, const std::shared_ptr<const Agent>& leading_agent, const double distance) {
+double BehaviorIDMClassic::CalcInteractionTerm(const double net_distance, const double vel_ego, const double vel_other) const {
 
   // Parameters
-  const float desired_velocity = get_desired_velocity();
   const float minimum_spacing = get_minimum_spacing();
   const float desired_time_headway = get_desired_time_headway();
   const float max_acceleration = get_max_acceleration();
   const float comfortable_braking_acceleration = get_comfortable_braking_acceleration();
+
+  const double net_velocity = vel_ego - vel_other;
+
+  const double helper_state = minimum_spacing + vel_ego*desired_time_headway +
+                      (vel_ego*net_velocity) / (2*sqrt(max_acceleration*comfortable_braking_acceleration));
+    
+  BARK_EXPECT_TRUE(!std::isnan(helper_state));
+  double interaction_term = (helper_state / net_distance)*(helper_state / net_distance);
+  BARK_EXPECT_TRUE(!std::isnan(interaction_term));
+
+  return interaction_term;
+
+}
+
+double BehaviorIDMClassic::CalcIDMAcc(const std::shared_ptr<const Agent>& ego_agent, const std::shared_ptr<const Agent>& leading_agent) {
+
+  const float desired_velocity = get_desired_velocity();
   const int exponent = get_exponent();
+  const float max_acceleration = get_max_acceleration();
 
   // relative velocity and longitudinal distance
   const State ego_state = ego_agent->GetCurrentState();
+  const auto& ego_lane_corridor = ego_agent->GetRoadCorridor()->GetCurrentLaneCorridor(ego_agent->GetCurrentPosition());
+  FrenetPosition frenet_ego(ego_agent->GetCurrentPosition(), ego_lane_corridor->GetCenterLine());
+
   const float ego_velocity = ego_state(StateDefinition::VEL_POSITION);
-  double interaction_term = 0.0f;
+
+  double interaction_term = 0;
+
   if(leading_agent) {
     // Leading vehicle exists in driving corridor, we calculate interaction term
+    const State leading_state = leading_agent->GetCurrentState();
+    const float other_velocity = leading_state(StateDefinition::VEL_POSITION);
 
-    const State lead_state = leading_agent->GetCurrentState();
+    const auto& leading_lane_corridor = leading_agent->GetRoadCorridor()->GetCurrentLaneCorridor(leading_agent->GetCurrentPosition());
+    FrenetPosition frenet_leading(leading_agent->GetCurrentPosition(), leading_lane_corridor->GetCenterLine());
 
     const float vehicle_length = ego_agent->GetShape().front_dist_ +
                                   leading_agent->GetShape().rear_dist_;
-    // TODO: that's not correct!
-    const double net_distance = distance - vehicle_length - 0.0f ; // For now assume ego longitudinal state at start of driving corridor
-    const double net_velocity = ego_state(StateDefinition::VEL_POSITION) - lead_state(StateDefinition::VEL_POSITION);
+    const double net_distance = frenet_leading.lon - vehicle_length - frenet_ego.lon;
 
-    const double helper_state = minimum_spacing + ego_velocity*desired_time_headway +
-                      (ego_velocity*net_velocity) / (2*sqrt(max_acceleration*comfortable_braking_acceleration));
-    
-    BARK_EXPECT_TRUE(!std::isnan(helper_state));
-    interaction_term = (helper_state / net_distance)*(helper_state / net_distance);
-    BARK_EXPECT_TRUE(!std::isnan(interaction_term));
+    interaction_term = CalcInteractionTerm(net_distance, ego_velocity, other_velocity);
   }
   
-  // Calculate acceleration based on https://en.wikipedia.org/wiki/Intelligent_driver_model (Maybe look into paper for other implementation aspects)  
+  // Calculate acceleration based on https://en.wikipedia.org/wiki/Intelligent_driver_model
   return max_acceleration * ( 1 - pow(ego_velocity / desired_velocity, exponent) - interaction_term);
 }
 
@@ -63,11 +81,9 @@ double BehaviorIDMClassic::CalculateLongitudinalAcceleration(const ObservedWorld
   std::pair<AgentPtr, FrenetPosition> leading_vehicle = observed_world.GetAgentInFront();
   std::shared_ptr<const Agent> ego_agent = observed_world.GetEgoAgent();
 
-  FrenetPosition lead_veh_frenet = leading_vehicle.second;
-  double distance = lead_veh_frenet.lon;
 
   // TODO(@Klemens): Distance refers to braking point, should be renamed / clarified
-  double acc = CalcLongAccTwoAgents(ego_agent, leading_vehicle.first, distance);
+  double acc = CalcIDMAcc(ego_agent, leading_vehicle.first);
   return acc;
 }
 
--- modules/models/behavior/idm/idm_classic.hpp
+++ modules/models/behavior/idm/idm_classic.hpp
@@ -22,21 +22,23 @@ class BehaviorIDMClassic : public BehaviorLongitudinalAcceleration {
 
   virtual ~BehaviorIDMClassic() {}
 
-  double CalcLongAccTwoAgents(
+  double CalcInteractionTerm(const double net_distance, const double vel_ego, const double vel_other) const;
+
+  double CalcIDMAcc(
       const std::shared_ptr<const world::objects::Agent>& ego_agent,
-      const std::shared_ptr<const world::objects::Agent>& leading_agent, const double distance);
+      const std::shared_ptr<const world::objects::Agent>& leading_agent);
 
   virtual double CalculateLongitudinalAcceleration(
       const world::ObservedWorld& observed_world);
 
-  const double get_desired_velocity() { return 15.0f; }  // unit is meter/second
-  const float get_minimum_spacing() { return 2.0f; }     // unit is meter
-  const float get_desired_time_headway() { return 1.5f; }  // unit is seconds
-  const float get_max_acceleration() { return 1.7f; }  // unit is meter/second^2
-  const float get_comfortable_braking_acceleration() {
+  const double get_desired_velocity() const { return 15.0f; }  // unit is meter/second
+  const float get_minimum_spacing() const { return 2.0f; }     // unit is meter
+  const float get_desired_time_headway() const { return 1.5f; }  // unit is seconds
+  const float get_max_acceleration() const { return 1.7f; }  // unit is meter/second^2
+  const float get_comfortable_braking_acceleration() const {
     return 1.67f;
   }  // unit is meter/second^2
-  const int get_exponent() { return 4; }
+  const int get_exponent() const { return 4; }
 
   virtual std::shared_ptr<BehaviorModel> Clone() const;
 };
