9dd9ad8b1cc8c9e3212b02ad5c830533775d6e52
Klemens Esterle <esterle@fortiss.org>
2020-02-04 19:36:51 +0100

    major refactoring of idm to have constant velocity assumotion for leading vehicle during delta_time

--- modules/models/behavior/idm/idm_classic.cpp
+++ modules/models/behavior/idm/idm_classic.cpp
@@ -1,12 +1,12 @@
-// Copyright (c) 2019 fortiss GmbH, Julian Bernhard, Klemens Esterle, Patrick Hart, Tobias Kessler
+// Copyright (c) 2019 fortiss GmbH, Julian Bernhard, Klemens Esterle, Patrick
+// Hart, Tobias Kessler
 //
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
-
 #include "modules/models/behavior/idm/idm_classic.hpp"
-#include "modules/world/observed_world.hpp"
 #include "modules/commons/transformation/frenet.hpp"
+#include "modules/world/observed_world.hpp"
 
 #include <math.h>
 
@@ -14,77 +14,170 @@ namespace modules {
 namespace models {
 namespace behavior {
 
+using modules::commons::transformation::FrenetPosition;
+using modules::geometry::Point2d;
 using modules::models::dynamic::State;
 using modules::models::dynamic::StateDefinition;
-using modules::world::objects::AgentPtr;
 using modules::world::objects::Agent;
-using modules::geometry::Point2d;
-using modules::commons::transformation::FrenetPosition;
+using modules::world::objects::AgentPtr;
 
-double BehaviorIDMClassic::CalcInteractionTerm(const double net_distance, const double vel_ego, const double vel_other) const {
+double BehaviorIDMClassic::CalcFreeRoadTerm(const double vel_ego) const {
+  const float desired_velocity = get_desired_velocity();
+  const int exponent = get_exponent();
 
+  double free_road_term = 1 - pow(vel_ego / desired_velocity, exponent);
+  return free_road_term;
+}
+
+double BehaviorIDMClassic::CalcInteractionTerm(const double net_distance,
+                                               const double vel_ego,
+                                               const double vel_other) const {
   // Parameters
   const float minimum_spacing = get_minimum_spacing();
   const float desired_time_headway = get_desired_time_headway();
   const float max_acceleration = get_max_acceleration();
-  const float comfortable_braking_acceleration = get_comfortable_braking_acceleration();
+  const float comfortable_braking_acceleration =
+      get_comfortable_braking_acceleration();
 
   const double net_velocity = vel_ego - vel_other;
 
-  const double helper_state = minimum_spacing + vel_ego*desired_time_headway +
-                      (vel_ego*net_velocity) / (2*sqrt(max_acceleration*comfortable_braking_acceleration));
-    
+  const double helper_state =
+      minimum_spacing + vel_ego * desired_time_headway +
+      (vel_ego * net_velocity) /
+          (2 * sqrt(max_acceleration * comfortable_braking_acceleration));
+
   BARK_EXPECT_TRUE(!std::isnan(helper_state));
-  double interaction_term = (helper_state / net_distance)*(helper_state / net_distance);
+  double interaction_term =
+      (helper_state / net_distance) * (helper_state / net_distance);
   BARK_EXPECT_TRUE(!std::isnan(interaction_term));
 
   return interaction_term;
-
 }
 
-double BehaviorIDMClassic::CalcIDMAcc(const std::shared_ptr<const Agent>& ego_agent, const std::shared_ptr<const Agent>& leading_agent) {
-
-  const float desired_velocity = get_desired_velocity();
-  const int exponent = get_exponent();
-  const float max_acceleration = get_max_acceleration();
-
+double BehaviorIDMClassic::CalcNetDistance(
+    const std::shared_ptr<const Agent>& ego_agent,
+    const std::shared_ptr<const Agent>& leading_agent) {
   // relative velocity and longitudinal distance
   const State ego_state = ego_agent->GetCurrentState();
-  const auto& ego_lane_corridor = ego_agent->GetRoadCorridor()->GetCurrentLaneCorridor(ego_agent->GetCurrentPosition());
-  FrenetPosition frenet_ego(ego_agent->GetCurrentPosition(), ego_lane_corridor->GetCenterLine());
+  FrenetPosition frenet_ego = ego_agent->CurrentFrenetPosition();
 
   const float ego_velocity = ego_state(StateDefinition::VEL_POSITION);
 
-  double interaction_term = 0;
+  // Leading vehicle exists in driving corridor, we calculate interaction term
+  const State leading_state = leading_agent->GetCurrentState();
+  const float other_velocity = leading_state(StateDefinition::VEL_POSITION);
 
-  if(leading_agent) {
-    // Leading vehicle exists in driving corridor, we calculate interaction term
-    const State leading_state = leading_agent->GetCurrentState();
-    const float other_velocity = leading_state(StateDefinition::VEL_POSITION);
+  FrenetPosition frenet_leading = leading_agent->CurrentFrenetPosition();
 
-    const auto& leading_lane_corridor = leading_agent->GetRoadCorridor()->GetCurrentLaneCorridor(leading_agent->GetCurrentPosition());
-    FrenetPosition frenet_leading(leading_agent->GetCurrentPosition(), leading_lane_corridor->GetCenterLine());
-
-    const float vehicle_length = ego_agent->GetShape().front_dist_ +
-                                  leading_agent->GetShape().rear_dist_;
-    const double net_distance = frenet_leading.lon - vehicle_length - frenet_ego.lon;
-
-    interaction_term = CalcInteractionTerm(net_distance, ego_velocity, other_velocity);
-  }
-  
-  // Calculate acceleration based on https://en.wikipedia.org/wiki/Intelligent_driver_model
-  return max_acceleration * ( 1 - pow(ego_velocity / desired_velocity, exponent) - interaction_term);
+  const float vehicle_length =
+      ego_agent->GetShape().front_dist_ + leading_agent->GetShape().rear_dist_;
+  const double net_distance =
+      frenet_leading.lon - vehicle_length - frenet_ego.lon;
+  return net_distance;
 }
 
-double BehaviorIDMClassic::CalculateLongitudinalAcceleration(const ObservedWorld& observed_world) {
+double BehaviorIDMClassic::CalcIDMAcc(const double net_distance,
+                                      const double vel_ego,
+                                      const double vel_other) const {
+  const float max_acceleration = get_max_acceleration();
+  const float acc = max_acceleration *
+                    (CalcFreeRoadTerm(vel_ego) -
+                     CalcInteractionTerm(net_distance, vel_ego, vel_other));
+  return acc;
+}
 
-  std::pair<AgentPtr, FrenetPosition> leading_vehicle = observed_world.GetAgentInFront();
+
+//! IDM Model will assume other front vehicle as constant velocity during delta_time
+Trajectory BehaviorIDMClassic::Plan(
+    float delta_time, const world::ObservedWorld& observed_world) {
+  std::pair<AgentPtr, FrenetPosition> leading_vehicle =
+      observed_world.GetAgentInFront();
   std::shared_ptr<const Agent> ego_agent = observed_world.GetEgoAgent();
 
+  using namespace dynamic;
+  //! TODO(@fortiss): parameters
+  const float min_velocity = GetMinVelocity();
+  const float max_velocity = GetMaxVelocity();
 
-  // TODO(@Klemens): Distance refers to braking point, should be renamed / clarified
-  double acc = CalcIDMAcc(ego_agent, leading_vehicle.first);
-  return acc;
+  const int num_traj_time_points = 11;
+  dynamic::Trajectory traj(num_traj_time_points,
+                           int(StateDefinition::MIN_STATE_SIZE));
+  float const dt = delta_time / (num_traj_time_points - 1);
+
+  dynamic::State ego_vehicle_state = observed_world.CurrentEgoState();
+
+  // select state and get p0
+  geometry::Point2d pose = observed_world.CurrentEgoPosition();
+
+  auto lane_corr = observed_world.GetLaneCorridor();
+  if (!lane_corr) {
+    this->SetLastTrajectory(traj);
+    return traj;
+  }
+
+  geometry::Line line = lane_corr->GetCenterLine();
+
+  double net_distance = .0f;
+  double vel_other = 1e6;
+  if (leading_vehicle.first) {
+    net_distance = CalcNetDistance(ego_agent, leading_vehicle.first);
+    dynamic::State other_vehicle_state =
+        leading_vehicle.first->GetCurrentState();
+    vel_other = other_vehicle_state(StateDefinition::VEL_POSITION);
+  }
+
+  if (line.obj_.size() > 0) {
+    // adding state at t=0
+    traj.block<1, StateDefinition::MIN_STATE_SIZE>(0, 0) =
+        ego_vehicle_state.transpose().block<1, StateDefinition::MIN_STATE_SIZE>(
+            0, 0);
+
+    float s_start = GetNearestS(line, pose);  // checked
+    double start_time = observed_world.GetWorldTime();
+    float vel_i = ego_vehicle_state(StateDefinition::VEL_POSITION);
+    float s_i = s_start;
+    double t_i;
+    double acc;
+    double traveled_ego;
+    double traveled_other;
+
+    for (int i = 1; i < num_traj_time_points; ++i) {
+      if (leading_vehicle.first) {
+        acc = CalcIDMAcc(net_distance, vel_i, vel_other);
+        traveled_ego = +0.5f * acc * dt * dt + vel_i * dt;
+        traveled_other = vel_other * dt;
+        net_distance += traveled_other - traveled_ego;
+      } else {
+        acc = get_max_acceleration() * CalcFreeRoadTerm(vel_i);
+      }
+
+      BARK_EXPECT_TRUE(!std::isnan(acc));
+
+      s_i += +0.5f * acc * dt * dt + vel_i * dt;
+      const float temp_velocity = vel_i + acc * dt;
+      vel_i = std::max(std::min(temp_velocity, max_velocity), min_velocity);
+      t_i = static_cast<float>(i) * dt + start_time;
+
+      geometry::Point2d traj_point = GetPointAtS(line, s_i);  // checked
+      float traj_angle = GetTangentAngleAtS(line, s_i);       // checked
+
+      BARK_EXPECT_TRUE(!std::isnan(boost::geometry::get<0>(traj_point)));
+      BARK_EXPECT_TRUE(!std::isnan(boost::geometry::get<1>(traj_point)));
+      BARK_EXPECT_TRUE(!std::isnan(traj_angle));
+
+      traj(i, StateDefinition::TIME_POSITION) = t_i;  // checked
+      traj(i, StateDefinition::X_POSITION) =
+          boost::geometry::get<0>(traj_point);  // checked
+      traj(i, StateDefinition::Y_POSITION) =
+          boost::geometry::get<1>(traj_point);                // checked
+      traj(i, StateDefinition::THETA_POSITION) = traj_angle;  // checked
+      traj(i, StateDefinition::VEL_POSITION) = vel_i;         // checked
+    }
+
+    SetLastAction(Action(Continuous1DAction(acc)));
+  }
+  this->SetLastTrajectory(traj);
+  return traj;
 }
 
 }  // namespace behavior
