71db85f83aa1aa740189a4fe1ac38c2792a328cf
jbernhard <bernhard@fortiss.org>
2020-02-05 11:25:10 +0100

    add child functionality for setter params

--- modules/commons/params/setter_params.hpp
+++ modules/commons/params/setter_params.hpp
@@ -17,7 +17,8 @@ namespace commons {
 // This class is mainly useful for test definitions in C++
 class SetterParams : public Params {
  public:
-  SetterParams(bool log_if_default=false) : params_bool_(), params_real_(), params_int_(),
+  SetterParams(bool log_if_default=false)
+                       : params_bool_(), params_real_(), params_int_(),
                          params_listlist_float_(), log_if_default_(log_if_default) {}
 
   virtual ~SetterParams() {}
@@ -48,21 +49,48 @@ class SetterParams : public Params {
 
   virtual int operator[](const std::string &param_name) { throw; } //< not supported atm 
 
-  virtual Params *AddChild(const std::string &name) { throw; } //< not supported atm
+  virtual Params *AddChild(const std::string &name) {
+      const auto it = childs_.find(name);
+      if(it != childs_.end()) {
+        return it->second.get();
+      }
+
+      std::shared_ptr<SetterParams> child(new SetterParams(log_if_default_));
+      childs_[name] = child;
+      return child.get();
+  }
+
+
 
   private: 
+
+    template<typename T> 
+    std::unordered_map<std::string, T> get_param_map();
+
+
     template<typename M, typename T>
     T get_parameter(M map, std::string param_name, T default_value) {
       const auto it = map.find(param_name);
       if(it != map.end()) {
         return it->second;
       } else {
-        if(log_if_default_) {
+        // find first child search there
+        std::string delimiter = "::";
+        auto pos = param_name.find(delimiter);
+        if (pos != std::string::npos) {
+          std::string child_name = param_name.substr(0, pos);
+          auto child_param = dynamic_cast<SetterParams*>(this->AddChild(child_name));
+          std::string child_param_name = param_name.erase(0, pos + delimiter.length());
+          return child_param->get_parameter(child_param->get_param_map<T>(), child_param_name, default_value);
+        }
+        if (log_if_default_) {
           LOG(INFO) << "Using default " << default_value <<" for param \"" << param_name << "\"";
         }
         return default_value;
       }
     }
+
+    std::unordered_map<std::string, std::shared_ptr<SetterParams>> childs_;
     std::unordered_map<std::string, bool> params_bool_;
     std::unordered_map<std::string, float> params_real_;
     std::unordered_map<std::string, int> params_int_;
@@ -71,6 +99,26 @@ class SetterParams : public Params {
     bool log_if_default_;
 
 };
+
+template <>
+std::unordered_map<std::string, bool> SetterParams::get_param_map() {
+  return params_bool_;
+}
+
+template <>
+std::unordered_map<std::string, float> SetterParams::get_param_map() {
+  return params_real_;
+}
+
+template <>
+std::unordered_map<std::string, int> SetterParams::get_param_map() {
+  return params_int_;
+}
+
+template <>
+std::unordered_map<std::string, std::vector<std::vector<float>>> SetterParams::get_param_map() {
+  return params_listlist_float_;
+}
 
 }  // namespace commons
 }  // namespace modules
