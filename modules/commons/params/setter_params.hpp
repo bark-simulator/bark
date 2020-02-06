d86b9ba65198e4b0247739138047a023291209e6
jbernhard <bernhard@fortiss.org>
2020-02-06 14:00:45 +0100

    parameter server pickling and unpickling interfaces

--- modules/commons/params/setter_params.hpp
+++ modules/commons/params/setter_params.hpp
@@ -10,14 +10,17 @@
 #include <string>
 #include <unordered_map>
 #include "modules/commons/params/params.hpp"
-
+#include "modules/commons/util/util.hpp"
 namespace modules {
 namespace commons {
 
 // This class is mainly useful for test definitions in C++
 class SetterParams : public Params {
  public:
-  SetterParams() : params_bool_(), params_real_(), params_int_(), params_listlist_float_() {}
+  SetterParams(bool log_if_default=false)
+                       : params_bool_(), params_real_(), params_int_(),
+                         params_listlist_float_(), log_if_default_(log_if_default) {}
+  SetterParams(bool log_if_default, const CondensedParamList& param_list);
 
   virtual ~SetterParams() {}
 
@@ -39,32 +42,120 @@ class SetterParams : public Params {
                       const std::vector<std::vector<float>> &default_value)  { return get_parameter(params_listlist_float_, param_name, default_value);}
 
   // not used atm
-  virtual void SetBool(const std::string &param_name, const bool &value) {params_bool_[param_name]=value;}
-  virtual void SetReal(const std::string &param_name, const float &value) {params_real_[param_name]=value;}
-  virtual void SetInt(const std::string &param_name, const int &value) {params_int_[param_name]=value;}
+  virtual void SetBool(const std::string &param_name, const bool &value) { set_parameter(params_bool_, param_name, value); }
+  virtual void SetReal(const std::string &param_name, const float &value) { set_parameter(params_real_, param_name, value); }
+  virtual void SetInt(const std::string &param_name, const int &value) { set_parameter(params_int_, param_name, value); }
   virtual void SetListListFloat(const std::string &param_name,
-                      const std::vector<std::vector<float>> &value) {params_listlist_float_[param_name]=value;}
+                      const std::vector<std::vector<float>> &value) { set_parameter(params_listlist_float_, param_name, value); }
+
+  virtual CondensedParamList GetCondensedParamList() { throw; } // < not needed atm
 
   virtual int operator[](const std::string &param_name) { throw; } //< not supported atm 
 
-  virtual Params *AddChild(const std::string &name) { throw; } //< not supported atm
+  virtual Params *AddChild(const std::string &name) {
+      const auto it = childs_.find(name);
+      if(it != childs_.end()) {
+        return it->second.get();
+      }
+
+      std::shared_ptr<SetterParams> child(new SetterParams(log_if_default_));
+      childs_[name] = child;
+      return child.get();
+  }
 
   private: 
+    template<typename T> 
+    std::unordered_map<std::string, T>& get_param_map();
+
     template<typename M, typename T>
-    T get_parameter(M map, std::string param_name, T default_value) {
+    void set_parameter(M& map, std::string param_name, T value) {
+      // find first child search there
+      std::string delimiter = "::";
+      auto pos = param_name.find(delimiter);
+      if (pos != std::string::npos) {
+        std::string child_name = param_name.substr(0, pos);
+        auto child_param = dynamic_cast<SetterParams*>(this->AddChild(child_name));
+        std::string child_param_name = param_name.erase(0, pos + delimiter.length());
+        child_param->set_parameter(child_param->get_param_map<T>(), child_param_name, value);
+        return;
+      }
+      map[param_name] =  value; // no child specification found, simply set value
+    }
+
+
+    template<typename M, typename T>
+    T get_parameter(M map, std::string param_name, const T& default_value) {
       const auto it = map.find(param_name);
       if(it != map.end()) {
         return it->second;
       } else {
+        // find first child search there
+        std::string delimiter = "::";
+        auto pos = param_name.find(delimiter);
+        if (pos != std::string::npos) {
+          std::string child_name = param_name.substr(0, pos);
+          auto child_param = dynamic_cast<SetterParams*>(this->AddChild(child_name));
+          std::string child_param_name = param_name.erase(0, pos + delimiter.length());
+          return child_param->get_parameter(child_param->get_param_map<T>(), child_param_name, default_value);
+        }
+        if (log_if_default_) {
+          LOG(INFO) << "Using default " << default_value <<" for param \"" << param_name << "\"";
+        }
         return default_value;
       }
     }
+
+    std::unordered_map<std::string, std::shared_ptr<SetterParams>> childs_;
     std::unordered_map<std::string, bool> params_bool_;
     std::unordered_map<std::string, float> params_real_;
     std::unordered_map<std::string, int> params_int_;
     std::unordered_map<std::string, std::vector<std::vector<float>>> params_listlist_float_;
 
+    bool log_if_default_;
+
 };
+
+struct ParamVisitor : public boost::static_visitor<> {
+  ParamVisitor(const SetterParams* params, const std::string& param_name) :
+         params_(params), param_name_(param_name) {}
+  void operator() (bool b) const { params_->SetBool(param_name_, b); }
+  void operator() (float r) const { params_->SetReal(param_name_, r); }
+  void operator() (int i) const { params_->SetInt(param_name_, i); }
+  void operator() (const ListListFloat& l) const { params_->SetListListFloat(param_name_, l); }
+
+private:
+  const SetterParams* params_;
+  const std::string& param_name_;
+}
+
+SetterParams::SetterParams(bool log_if_default, const CondensedParamList& param_list) {
+  for(const& ParamPair param_pair : param_list) {
+    const auto& param_name = param_pair.first;
+    const auto& param_variant = param_pair.second;
+    boost::apply_visitor(ParamVisitor(this, param_name), param_variant);
+  }
+}
+
+
+template <>
+std::unordered_map<std::string, bool>& SetterParams::get_param_map() {
+  return params_bool_;
+}
+
+template <>
+std::unordered_map<std::string, float>& SetterParams::get_param_map() {
+  return params_real_;
+}
+
+template <>
+std::unordered_map<std::string, int>& SetterParams::get_param_map() {
+  return params_int_;
+}
+
+template <>
+std::unordered_map<std::string, std::vector<std::vector<float>>>& SetterParams::get_param_map() {
+  return params_listlist_float_;
+}
 
 }  // namespace commons
 }  // namespace modules
---
+++ modules/runtime/tests/py_param_server_tests.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2019 fortiss GmbH
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+from modules.runtime.commons.parameters import ParameterServer
+
+
+class ParamServerTests(unittest.TestCase):
+  def test_serialize(self):
+    pass
+
+
+if __name__ == '__main__':
+  unittest.main()
