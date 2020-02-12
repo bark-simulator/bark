4ceccf585e280444f799b102cb2afd6baeb2354c
juloberno <juloberno@gmail.com>
2020-02-12 21:38:59 +0100

    make params behavior configurable

--- modules/runtime/scenario/scenario_generation/config_readers/behavior_model_config_readers.py
+++ modules/runtime/scenario/scenario_generation/config_readers/behavior_model_config_readers.py
@@ -18,17 +18,18 @@ class FixedBehaviorType(ConfigReaderBehaviorModels):
   def create_from_config(self, config_param_object, road_corridor, agent_states,  **kwargs):
     model_type = config_param_object["ModelType", "Type of behavior model \
                 used for all vehicles", "BehaviorIDMClassic"]
+    model_params = config_param_object.AddChild("ModelParams")
+    self.param_servers.append(model_params) # use the same param server for all models
     behavior_models = []
     behavior_model_types = []
+    model_default_params = ParameterServer(json={})
     for _ in agent_states:
-      bark_model, params = self.model_from_model_type(model_type)
+      bark_model, params = self.model_from_model_type(model_type, model_params)
       behavior_models.append(bark_model)
-      self.param_servers.append(params)
       behavior_model_types.append(model_type)
     return behavior_models, {"behavior_model_types" : behavior_model_types}, config_param_object
 
-  def model_from_model_type(self, model_type):
-    params = ParameterServer()
+  def model_from_model_type(self, model_type, params):
     bark_model = eval("{}(params)".format(model_type))    
     return bark_model, params
 
