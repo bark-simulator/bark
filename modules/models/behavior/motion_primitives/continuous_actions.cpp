2c52d3f0c753fda73b4638067d2882b66a71ee3d
Klemens Esterle <esterle@fortiss.org>
2020-02-12 12:55:15 +0100

    Continous Actions Class deriving from Motion Primitives Class

---
+++ modules/models/behavior/motion_primitives/continuous_actions.cpp
@@ -0,0 +1,21 @@
+// Copyright (c) 2019 fortiss GmbH, Julian Bernhard, Klemens Esterle, Patrick
+// Hart, Tobias Kessler
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
+
+#include "modules/models/behavior/motion_primitives/continuous_actions.hpp"
+
+namespace modules {
+namespace models {
+namespace behavior {
+  
+BehaviorMotionPrimitives::MotionIdx
+BehaviorMPContinousActions::AddMotionPrimitive(const Input& dynamic_input) {
+  motion_primitives_.push_back(dynamic_input);
+  return motion_primitives_.size() - 1;
+}
+
+}  // namespace behavior
+}  // namespace models
+}  // namespace modules
--- modules/models/behavior/motion_primitives/motion_primitives.hpp
+++ modules/models/behavior/motion_primitives/motion_primitives.hpp
@@ -4,8 +4,8 @@
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
-#ifndef MODULES_MODELS_BEHAVIOR_MOTION_PRIMITIVES_HPP_
-#define MODULES_MODELS_BEHAVIOR_MOTION_PRIMITIVES_HPP_
+#ifndef MODULES_MODELS_BEHAVIOR_MOTION_PRIMITIVES_MOTION_PRIMITIVES_HPP_
+#define MODULES_MODELS_BEHAVIOR_MOTION_PRIMITIVES_MOTION_PRIMITIVES_HPP_
 
 #include "modules/models/behavior/behavior_model.hpp"
 #include "modules/models/dynamic/dynamic_model.hpp"
@@ -32,29 +32,20 @@ class BehaviorMotionPrimitives : public BehaviorModel {
                           const ObservedWorld& observed_world);
 
   typedef unsigned int MotionIdx;
-  MotionIdx AddMotionPrimitive(const Input& dynamic_input);
-  MotionIdx GetNumMotionPrimitives() const { return motion_primitives_.size(); }
+  virtual MotionIdx GetNumMotionPrimitives() const = 0;
+  virtual Input GetAction() const = 0;
   void ActionToBehavior(const MotionIdx& motion_idx);
 
-  virtual std::shared_ptr<BehaviorModel> Clone() const;
-
- private:
+ protected:
   DynamicModelPtr dynamic_model_;
-  std::vector<Input> motion_primitives_;
   MotionIdx active_motion_;
 
   // Parameters
   float integration_time_delta_;
 };
 
-inline std::shared_ptr<BehaviorModel> BehaviorMotionPrimitives::Clone() const {
-  std::shared_ptr<BehaviorMotionPrimitives> model_ptr =
-      std::make_shared<BehaviorMotionPrimitives>(*this);
-  return std::dynamic_pointer_cast<BehaviorModel>(model_ptr);
-}
-
 }  // namespace behavior
 }  // namespace models
 }  // namespace modules
 
-#endif  // MODULES_MODELS_BEHAVIOR_MOTION_PRIMITIVES_HPP_
+#endif  // MODULES_MODELS_BEHAVIOR_MOTION_PRIMITIVES_MOTION_PRIMITIVES_HPP_
