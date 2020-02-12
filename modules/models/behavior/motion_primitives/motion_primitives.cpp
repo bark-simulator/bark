017b3901662f737bac8b7db922f472afe324f0f5
Klemens Esterle <esterle@fortiss.org>
2020-02-12 10:03:15 +0100

    cleaned up plan method of motion primitives

--- modules/models/behavior/motion_primitives/motion_primitives.cpp
+++ modules/models/behavior/motion_primitives/motion_primitives.cpp
@@ -1,8 +1,8 @@
-// Copyright (c) 2019 fortiss GmbH, Julian Bernhard, Klemens Esterle, Patrick Hart, Tobias Kessler
+// Copyright (c) 2019 fortiss GmbH, Julian Bernhard, Klemens Esterle, Patrick
+// Hart, Tobias Kessler
 //
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
-
 
 #include "modules/models/behavior/motion_primitives/motion_primitives.hpp"
 #include "modules/models/dynamic/integration.hpp"
@@ -14,58 +14,38 @@ namespace behavior {
 
 using modules::models::dynamic::StateDefinition;
 
-BehaviorMotionPrimitives::BehaviorMotionPrimitives(const DynamicModelPtr& dynamic_model, commons::Params *params) : 
-    BehaviorModel(params),
-    dynamic_model_(dynamic_model),
-    motion_primitives_(),
-    active_motion_(),
-    integration_time_delta_(params->GetReal("integration_time_delta",
-                                             "the size of the time steps used within the euler integration loop", 0.02))
-    {}
+BehaviorMotionPrimitives::BehaviorMotionPrimitives(
+    const DynamicModelPtr& dynamic_model, commons::Params* params)
+    : BehaviorModel(params),
+      dynamic_model_(dynamic_model),
+      motion_primitives_(),
+      active_motion_(),
+      integration_time_delta_(params->GetReal(
+          "integration_time_delta",
+          "the size of the time steps used within the euler integration loop",
+          0.02)) {}
 
-dynamic::Trajectory BehaviorMotionPrimitives::Plan(
+Trajectory BehaviorMotionPrimitives::Plan(
     float delta_time, const world::ObservedWorld& observed_world) {
-  dynamic::State ego_vehicle_state = observed_world.CurrentEgoState();
-  double start_time = observed_world.GetWorldTime();
   const float dt = integration_time_delta_;
   const int num_trajectory_points =
       static_cast<int>(std::ceil(delta_time / dt)) + 1;
 
-  dynamic::Trajectory traj(num_trajectory_points, int(dynamic::StateDefinition::MIN_STATE_SIZE));
-
-  
-  int traj_idx=0;
-  // set first state as start of trajectory
-  traj(traj_idx, StateDefinition::TIME_POSITION) = start_time;
-  traj(traj_idx, StateDefinition::X_POSITION) = ego_vehicle_state(StateDefinition::X_POSITION);
-  traj(traj_idx, StateDefinition::Y_POSITION) = ego_vehicle_state(StateDefinition::Y_POSITION);
-  traj(traj_idx, StateDefinition::THETA_POSITION) = ego_vehicle_state(StateDefinition::THETA_POSITION);
-  traj(traj_idx, StateDefinition::VEL_POSITION) = ego_vehicle_state(StateDefinition::VEL_POSITION); 
-  
-  auto old_state = ego_vehicle_state; // only for getting type, todo: improve
+  Trajectory traj(num_trajectory_points,
+                  static_cast<int>(StateDefinition::MIN_STATE_SIZE));
+  traj.row(0) = observed_world.CurrentEgoState();
 
   float integration_time;
-  for (++traj_idx; traj_idx<num_trajectory_points; ++traj_idx) {
-    old_state(StateDefinition::TIME_POSITION) = traj(traj_idx-1, StateDefinition::TIME_POSITION);
-    old_state(StateDefinition::X_POSITION) = traj(traj_idx-1, StateDefinition::X_POSITION);
-    old_state(StateDefinition::Y_POSITION) = traj(traj_idx-1, StateDefinition::Y_POSITION);
-    old_state(StateDefinition::THETA_POSITION) = traj(traj_idx-1, StateDefinition::THETA_POSITION);
-    old_state(StateDefinition::VEL_POSITION) = traj(traj_idx-1, StateDefinition::VEL_POSITION);
-
-    if (traj_idx == num_trajectory_points - 1) {
+  for (int i = 1; i < num_trajectory_points; ++i) {
+    if (i == num_trajectory_points - 1) {
       // calculate the last time pt, which might not fit to dt
-      integration_time = delta_time - (traj_idx - 1) * dt;
-    }
-    else {
+      integration_time = delta_time - (i - 1) * dt;
+    } else {
       integration_time = dt;
     }
-
-    auto state = dynamic::euler_int(*dynamic_model_, old_state, motion_primitives_[active_motion_], integration_time);
-    traj(traj_idx, StateDefinition::TIME_POSITION) = start_time + (traj_idx - 1) * dt + integration_time;
-    traj(traj_idx, StateDefinition::X_POSITION) = state(StateDefinition::X_POSITION);
-    traj(traj_idx, StateDefinition::Y_POSITION) = state(StateDefinition::Y_POSITION);
-    traj(traj_idx, StateDefinition::THETA_POSITION) = state(StateDefinition::THETA_POSITION);
-    traj(traj_idx, StateDefinition::VEL_POSITION) = state(StateDefinition::VEL_POSITION);
+    traj.row(i) = dynamic::euler_int(*dynamic_model_, traj.row(i - 1),
+                                     motion_primitives_[active_motion_],
+                                     integration_time);
   }
 
   SetLastAction(Action(DiscreteAction(active_motion_)));
@@ -74,9 +54,10 @@ dynamic::Trajectory BehaviorMotionPrimitives::Plan(
   return traj;
 }
 
-BehaviorMotionPrimitives::MotionIdx BehaviorMotionPrimitives::AddMotionPrimitive(const Input& dynamic_input) {  
+BehaviorMotionPrimitives::MotionIdx
+BehaviorMotionPrimitives::AddMotionPrimitive(const Input& dynamic_input) {
   motion_primitives_.push_back(dynamic_input);
-  return motion_primitives_.size()-1;
+  return motion_primitives_.size() - 1;
 }
 
 void BehaviorMotionPrimitives::ActionToBehavior(const MotionIdx& motion_idx) {
--- modules/models/behavior/motion_primitives/motion_primitives.hpp
+++ modules/models/behavior/motion_primitives/motion_primitives.hpp
@@ -1,8 +1,8 @@
-// Copyright (c) 2019 fortiss GmbH, Julian Bernhard, Klemens Esterle, Patrick Hart, Tobias Kessler
+// Copyright (c) 2019 fortiss GmbH, Julian Bernhard, Klemens Esterle, Patrick
+// Hart, Tobias Kessler
 //
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
-
 
 #ifndef MODULES_MODELS_BEHAVIOR_STATE_DELTA_STATE_DELTA_HPP_
 #define MODULES_MODELS_BEHAVIOR_STATE_DELTA_STATE_DELTA_HPP_
@@ -14,32 +14,33 @@ namespace modules {
 namespace models {
 namespace behavior {
 
-using dynamic::Trajectory;
-using dynamic::State;
 using dynamic::DynamicModelPtr;
 using dynamic::Input;
-using world::objects::AgentId;
+using dynamic::State;
+using dynamic::Trajectory;
 using world::ObservedWorld;
+using world::objects::AgentId;
 
 class BehaviorMotionPrimitives : public BehaviorModel {
  public:
   BehaviorMotionPrimitives(const DynamicModelPtr& dynamic_model,
-                           commons::Params *params);
+                           commons::Params* params);
 
   virtual ~BehaviorMotionPrimitives() {}
 
   virtual Trajectory Plan(float delta_time,
-                 const ObservedWorld& observed_world);
+                          const ObservedWorld& observed_world);
 
   typedef unsigned int MotionIdx;
   MotionIdx AddMotionPrimitive(const Input& dynamic_input);
-  MotionIdx GetNumMotionPrimitives() const {return motion_primitives_.size();}
+  MotionIdx GetNumMotionPrimitives() const { return motion_primitives_.size(); }
   void ActionToBehavior(const MotionIdx& motion_idx);
 
   virtual std::shared_ptr<BehaviorModel> Clone() const;
-private:
+
+ private:
   DynamicModelPtr dynamic_model_;
-  std::vector<Input> motion_primitives_; 
+  std::vector<Input> motion_primitives_;
   MotionIdx active_motion_;
 
   // Parameters
@@ -48,7 +49,7 @@ private:
 
 inline std::shared_ptr<BehaviorModel> BehaviorMotionPrimitives::Clone() const {
   std::shared_ptr<BehaviorMotionPrimitives> model_ptr =
-    std::make_shared<BehaviorMotionPrimitives>(*this);
+      std::make_shared<BehaviorMotionPrimitives>(*this);
   return std::dynamic_pointer_cast<BehaviorModel>(model_ptr);
 }
 
