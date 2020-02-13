d844b6d0475e5ba1879adc075ed502af6eddec4d
jbernhard <bernhard@fortiss.org>
2020-02-13 17:00:55 +0100

    fix error due to missing python includes

--- tools/installers/install_python_venv.sh
+++ tools/installers/install_python_venv.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # we need root access for that, should thus only be called in docker file
 apt-get update
-apt-get install -y python3-pip python3-dev python3-tk
+apt-get install -y python3.7-dev python3.7-tk #python3.7-venv
+
+# Do not update virtualenv to new version, if not guaranteed that python headers are symlinked
+pip3 install virtualenv==16.7.2 
+
+pip3 install --upgrade pip
