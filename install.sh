61e717e1c58f4867afd019d47c062d8bba418f3e
jbernhard <bernhard@fortiss.org>
2020-02-13 17:00:55 +0100

    fix error due to missing python includes

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-virtualenv -p python3.7 ./python/venv
+virtualenv -p python3.7 ./python/venv --system-site-packages
 source ./python/venv/bin/activate && pip install -r tools/installers/requirements.txt
