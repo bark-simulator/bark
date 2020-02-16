6415b4b09751aeb8c0b88c012b7e81a93f897bdc
juloberno <juloberno@gmail.com>
2020-02-16 20:18:23 +0100

    multiprocessing test working

--- modules/benchmark/benchmark_runner_mp.py
+++ modules/benchmark/benchmark_runner_mp.py
@@ -3,22 +3,34 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-
+import pickle 
 import ray
 import logging
 import psutil
+import inspect
+
+import bark.world.evaluation
 logging.getLogger().setLevel(logging.INFO)
 
-from modules.benchmark.benchmark_runner import BenchmarkRunner, BenchmarkResult
+from modules.benchmark.benchmark_runner import BenchmarkRunner, BenchmarkResult, BenchmarkConfig
 
 # implement a parallelized version of benchmark running based on ray
+
+# custom serialization as cloudpickling fails
+def serialize_benchmark_config(bc):
+  return pickle.dumps(bc)
+
+def deserialize_benchmark_config(bc):
+  return pickle.loads(bc)
+
 
 # actor class running on a single core
 @ray.remote
 class _BenchmarkRunnerActor(BenchmarkRunner):
-  def __init__(self, benchmark_configs, core_idx):
-        super().__init__(benchmark_configs=benchmark_configs)
-        self.core_idx = core_idx
+  def __init__(self, evaluators, terminal_when, benchmark_configs):
+        super().__init__(evaluators=evaluators, 
+                          terminal_when=terminal_when,
+                          benchmark_configs=benchmark_configs)
 
 # runner spawning actors and distributing benchmark configs
 class BenchmarkRunnerMP(BenchmarkRunner):
@@ -30,16 +42,25 @@ class BenchmarkRunnerMP(BenchmarkRunner):
                benchmark_configs=None, num_cpus=None):
         super().__init__(benchmark_database=benchmark_database,
                           evaluators=evaluators, terminal_when=terminal_when,
-                          behaviors=behaviors, benchmark_configs=benchmark_configs,
-                          num_scenarios=num_scenarios)
+                          behaviors=behaviors, num_scenarios=num_scenarios,
+                          benchmark_configs=benchmark_configs,
+                          )
         num_cpus_available = psutil.cpu_count(logical=True)
         if num_cpus and num_cpus <= num_cpus_available:
             pass
         else:
             num_cpus = num_cpus_available
         ray.init(num_cpus=num_cpus)
+        ray.register_custom_serializer(
+          BenchmarkConfig, serializer=serialize_benchmark_config,
+          deserializer=deserialize_benchmark_config)
+        ray.register_custom_serializer(
+          BenchmarkConfig, serializer=serialize_benchmark_config,
+          deserializer=deserialize_benchmark_config)
         self.benchmark_config_split = [self.benchmark_configs[i::num_cpus] for i in range(0, num_cpus)]
-        self.actors = [_BenchmarkRunnerActor.remote(self.benchmark_config_split[i], i) for i in range(num_cpus) ]
+        self.actors = [_BenchmarkRunnerActor.remote(evaluators=evaluators,
+                                                    terminal_when=terminal_when,
+                                                    benchmark_configs=self.benchmark_config_split[i]) for i in range(num_cpus) ]
 
     def run(self):
         results_tmp = ray.get([actor.run.remote() for actor in self.actors])
@@ -49,4 +70,4 @@ class BenchmarkRunnerMP(BenchmarkRunner):
             result_dict.extend(result_tmp.get_result_dict())
             benchmark_configs.extend(result_tmp.get_benchmark_configs())
         ray.shutdown()
-        return BenchmarkResult(result_dict, eval_configs)
+        return BenchmarkResult(result_dict, benchmark_configs)
--- modules/benchmark/tests/py_benchmark_runner_tests.py
+++ modules/benchmark/tests/py_benchmark_runner_tests.py
@@ -5,11 +5,13 @@
 
 import unittest
 import os
+import ray
 
 from load.benchmark_database import BenchmarkDatabase
 from serialization.database_serializer import DatabaseSerializer
 from modules.benchmark.benchmark_runner import BenchmarkRunner, BenchmarkConfig
-from modules.benchmark.benchmark_runner_mp import BenchmarkRunnerMP
+from modules.benchmark.benchmark_runner_mp import BenchmarkRunnerMP, _BenchmarkRunnerActor, \
+        deserialize_benchmark_config, serialize_benchmark_config
 
 from modules.runtime.scenario.scenario_generation.configurable_scenario_generation \
   import ConfigurableScenarioGeneration
@@ -44,20 +46,48 @@ class DatabaseRunnerTests(unittest.TestCase):
         df = result.get_data_frame()
         print(df)
         self.assertEqual(len(df.index), 20) # 2 Behaviors * 10 Serialize Scenarios
+    @unittest.skip
+    def test_serialization_benchmark_config(self):
+      params = ParameterServer()
+      scenario_generation = ConfigurableScenarioGeneration(num_scenarios=4,params=params)
+      bc = BenchmarkConfig(0, BehaviorConstantVelocity(params),
+                  "const", scenario_generation.get_scenario(0), 0, "test1")
+      deserialized_bc =  deserialize_benchmark_config(serialize_benchmark_config(bc))
+      self.assertEqual(deserialized_bc.behavior_name, "const")
+      self.assertTrue(isinstance(deserialized_bc.behavior, BehaviorConstantVelocity))
+    @unittest.skip
+    def test_ray_serialization_benchmark_config(self):
+      ray.init(ignore_reinit_error=True)
+      ray.register_custom_serializer(
+          BenchmarkConfig, serializer=serialize_benchmark_config,
+          deserializer=deserialize_benchmark_config)
+      params = ParameterServer()
+      scenario_generation = ConfigurableScenarioGeneration(num_scenarios=4,params=params)
+      bc = BenchmarkConfig(0, BehaviorConstantVelocity(params),
+                  "const", scenario_generation.get_scenario(0), 0, "test1")
+      object_id = ray.put(bc)
+      self.assertEqual(ray.get(object_id).behavior_name, "const")
+      self.assertTrue(isinstance(ray.get(object_id).behavior, BehaviorConstantVelocity))
+      ray.shutdown()
 
     def test_database_multiprocessing_runner(self):
-        params = ParameterServer()
-        scenario_generation = ConfigurableScenarioGeneration(num_scenarios=4,params=params)
-        benchmark_configs = [BenchmarkConfig(0, BehaviorConstantVelocity(params),
-                  "const", scenario_generation.get_scenario(0), 0, "test1"),
-                BenchmarkConfig(1, BehaviorConstantVelocity(params),
-                  "const", scenario_generation.get_scenario(1), 1, "test1"),
-                BenchmarkConfig(2, BehaviorIDMClassic(params),
-                  "IDM", scenario_generation.get_scenario(2), 2, "test1"),
-                BenchmarkConfig(3, BehaviorIDMClassic(params),
-                  "IDM", scenario_generation.get_scenario(3), 3, "test1")]
+        dbs = DatabaseSerializer(test_scenarios=4, test_world_steps=5, num_serialize_scenarios=10)
+        os.chdir("../benchmark_database/")
+        dbs.process("database")
+        local_release_filename = dbs.release(version="test")
 
-        benchmark_runner = BenchmarkRunnerMP(benchmark_configs=benchmark_configs)
+        db = BenchmarkDatabase(database_root=local_release_filename)
+        evaluators = {"success" : "EvaluatorGoalReached", "collision" : "EvaluatorCollisionEgoAgent",
+                      "max_steps": "EvaluatorStepCount"}
+        terminal_when = {"collision" :lambda x: x, "max_steps": lambda x : x>2}
+        params = ParameterServer() # only for evaluated agents not passed to scenario!
+        behaviors_tested = {"IDM": BehaviorIDMClassic(params), "Const" : BehaviorConstantVelocity(params)}
+                                        
+
+        benchmark_runner = BenchmarkRunnerMP(benchmark_database=db,
+                                           evaluators=evaluators,
+                                           terminal_when=terminal_when,
+                                           behaviors=behaviors_tested)
 
         result = benchmark_runner.run()
         
