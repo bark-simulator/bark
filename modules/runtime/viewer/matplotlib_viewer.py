d6a0825f433253c4f53eaaa80be86cc5a45d3b43
Klemens Esterle <esterle@fortiss.org>
2020-02-25 13:51:09 +0100

    fixing get_ax_size

--- modules/runtime/viewer/matplotlib_viewer.py
+++ modules/runtime/viewer/matplotlib_viewer.py
@@ -85,7 +85,7 @@ class MPViewer(BaseViewer):
       return (w/h)
 
     def _get_ax_size(self):
-        scale = fig.dpi_scale_trans.inverted()
+        scale = self.axes.get_figure().dpi_scale_trans.inverted()
         bbox = self.axes.get_window_extent().transformed(scale)
         width, height = bbox.width, bbox.height
         width *= fig.dpi
