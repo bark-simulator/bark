398a6552221b4ae1f6c9fb1d922c4bf2cff0baf2
Patrick Hart <patrickhart.1990@gmail.com>
2020-02-28 17:44:21 +0100

    Added python bindings

--- modules/world/observed_world.hpp
+++ modules/world/observed_world.hpp
@@ -124,6 +124,7 @@ class ObservedWorld : public World {
         std::make_shared<ObservedWorld>(world_clone, this->ego_agent_id_);
     return std::dynamic_pointer_cast<World>(observed_world);
   }
+  virtual EvaluationMap Evaluate() const;
 
  private:
   AgentId ego_agent_id_;
--- python/world/world.cpp
+++ python/world/world.cpp
@@ -66,8 +66,7 @@ void python_world(py::module m) {
   py::class_<ObservedWorld, World, std::shared_ptr<ObservedWorld>>(m, "ObservedWorld")
     .def(py::init<const WorldPtr&, const AgentId&>())
     .def_property_readonly("ego_agent", &ObservedWorld::GetEgoAgent)
-    .def_property_readonly("agents", &World::GetAgents)
-    .def("Evaluate", &World::Evaluate)
+    .def("Evaluate", &ObservedWorld::Evaluate)
     .def("PredictWithOthersIDM",
       &ObservedWorld::Predict<BehaviorIDMClassic, DynamicBehaviorModel>)
     .def_property_readonly("other_agents", &ObservedWorld::GetOtherAgents)
