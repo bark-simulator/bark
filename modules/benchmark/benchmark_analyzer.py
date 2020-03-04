e28aa44dd3b061a11411519d15f2f38902c2e10f
jbernhard <bernhard@fortiss.org>
2020-03-04 13:30:41 +0100

    benchmark analyzer visualize multiples

--- modules/benchmark/benchmark_analyzer.py
+++ modules/benchmark/benchmark_analyzer.py
@@ -36,34 +36,75 @@ class BenchmarkAnalyzer:
       configs_found.sort()
       return configs_found
 
-  def visualize(self, configs_idx_list, viewer, real_time_factor=1.0, display_info=True, **kwargs):
-      for config_idx in configs_idx_list:
-          benchmark_config = self._benchmark_result.get_benchmark_config(config_idx)
-          histories = self._benchmark_result.get_history(config_idx)
-          if len(histories) == 0:
-             logging.info("No histories for config idx {}. Skipping....".format(config_idx))
+  def visualize(self, configs_idx_list, viewer, viewer_names=None, real_time_factor=1.0, display_info=True, **kwargs):
+      if not all(isinstance(elem, list) for elem in configs_idx_list):
+          configs_idx_list = [configs_idx_list]
+      if not isinstance(viewer, list):
+          viewer = [viewer]
+      if not len(configs_idx_list) == len(viewer):
+          raise ValueError("Equal number of viewers and configs_idx_list required.")
 
-          if real_time_factor:
-              try:
-                  sim_time = benchmark_config.scenario.json_params["Simulation"]["StepTime"]
-              except:
-                  logging.warning("Simulation step time not privided for config {}. Using default 0.2s".format(config_idx))
-                  sim_time = 0.2
-          step_time =  sim_time/real_time_factor
-          world_time = 0
-          for scenario in histories:
-              world = scenario.get_world_state()
-              world.time = world_time
-              if display_info:
-                  info_text_list = benchmark_config.get_info_string_list()
-                  info_text = " | ".join(info_text_list)
-                  viewer.drawText(text=info_text, position=(0.5,1.05), **kwargs)
-              viewer.drawWorld(world = world,
-                          eval_agent_ids = benchmark_config.scenario.eval_agent_ids, \
-                          scenario_idx = None, debug_text=False)
+      num_configs = len(configs_idx_list[0])
+      num_parallel_visualizations = len(viewer)
+      for config_list_idx in range(0, num_configs):
+          # collect histories
+          histories_collected = {}
+          info_strings_collected = {}
+          sim_times_collected = {}
+          for viewer_idx in range(0, num_parallel_visualizations):
+              config_idx = configs_idx_list[viewer_idx][config_list_idx]
+              benchmark_config = self._benchmark_result.get_benchmark_config(config_idx)
+              histories = self._benchmark_result.get_history(config_idx)
+              histories_collected[viewer_idx] = histories
 
-              viewer.show()
-              world_time += sim_time
+              if len(histories) == 0:
+                logging.info("No histories for config idx {}. Skipping....".format(config_idx))
+
               if real_time_factor:
-                  time.sleep(step_time)
-              viewer.clear()
+                  try:
+                      sim_time = benchmark_config.scenario.json_params["Simulation"]["StepTime"]
+                  except:
+                      logging.warning("Simulation step time not privided for config {}. Using default 0.2s".format(config_idx))
+                      sim_time = 0.2
+
+              info_text_list = benchmark_config.get_info_string_list()
+              info_text = " | ".join(info_text_list)
+              info_strings_collected[viewer_idx] = info_text
+
+              sim_times_collected[viewer_idx] = sim_time
+
+          self._render_histories(histories_collected, info_strings_collected, viewer,
+                               viewer_names, sim_time, real_time_factor, display_info,  **kwargs)
+
+
+  def _render_histories(self, histories_collected, info_strings_collected, viewer_list,
+                        viewer_names, sim_time, real_time_factor, display_info, **kwargs):
+          world_time = 0
+          step_time = sim_time/real_time_factor
+          steps_to_go = True
+          step = 0
+          while steps_to_go:
+              step += 1
+              steps_to_go = False
+              for viewer_idx in range(0, len(viewer_list)):
+                  histories = histories_collected[viewer_idx]
+                  if len(histories) < step:
+                    continue
+                  steps_to_go = True
+                  scenario = histories[step-1]
+                  viewer = viewer_list[viewer_idx]
+                  world = scenario.get_world_state()
+                  world.time = world_time
+                  if display_info and not viewer_names:
+                      info_text = info_strings_collected[viewer_idx]
+                      viewer.drawText(text=info_text, position=(0.5,1.05), **kwargs)
+                  elif viewer_names:
+                      viewer.drawText(text=viewer_names[viewer_idx], position=(0.5,1.05), **kwargs)
+                  viewer.drawWorld(world = world,
+                              eval_agent_ids = scenario.eval_agent_ids, \
+                              scenario_idx = None, debug_text=False)
+                  viewer.show()
+                  world_time += sim_time
+                  if real_time_factor:
+                      time.sleep(step_time)
+                  viewer.clear()
