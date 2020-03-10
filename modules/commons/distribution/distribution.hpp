15395b082966bae7f71fc33c45f45f64204c8793
jbernhard <bernhard@fortiss.org>
2020-03-10 12:55:35 +0100

    behavior hypothesis and distribution base class

--- modules/commons/distribution/distribution.hpp
+++ modules/commons/distribution/distribution.hpp
@@ -1,10 +1,34 @@
+// Copyright (c) 2019 fortiss GmbH, Julian Bernhard, Klemens Esterle, Patrick
+// Hart, Tobias Kessler
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
 
+#ifndef MODULES_COMMONS_DISTRIBUTION_HPP_
+#define MODULES_COMMONS_DISTRIBUTION_HPP_
+
+
+#include <Eigen/Core>
+
+namespace modules {
+namespace commons {
 
 typedef double Probability;
-typedef  Eigen::Matrix
+typedef float RandomVariableSupport;
+typedef Eigen::Matrix<RandomVariableSupport, Eigen::Dynamic, 1> RandomSample;
 
-class Distribution {
-   Distribution(ParamsPtr) {}
+class Distribution : public BaseType{
+  public:
+    Distribution(const ParamsPtr& params) : BaseType(params) {}
 
+    virtual RandomSample sample() = 0;
 
+    virtual Probability GetProbability(const RandomSample& random_sample) const = 0;
 };
+
+
+
+}  // namespace commons
+}  // namespace modules
+
+#endif  // MODULES_COMMONS_DISTRIBUTION_HPP_
---
+++ modules/models/behavior/hypothesis/behavior_hypothesis.hpp
@@ -0,0 +1,41 @@
+// Copyright (c) 2019 fortiss GmbH, Julian Bernhard, Klemens Esterle, Patrick Hart, Tobias Kessler
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
+
+
+#ifndef MODULES_MODELS_BEHAVIOR_BEHAVIOR_HYPOTHESIS_HPP_
+#define MODULES_MODELS_BEHAVIOR_BEHAVIOR_HYPOTHESIS_HPP_
+
+#include "modules/models/behavior/behavior_model.hpp"
+#include "modules/commons/distribution/distribution.hpp"
+
+namespace modules {
+namespace world {
+namespace objects {
+class Agent;
+typedef std::shared_ptr<Agent> AgentPtr;
+typedef unsigned int AgentId;
+}  // namespace objects
+class ObservedWorld;
+}  // namespace world
+namespace models {
+namespace behavior {
+
+
+class BehaviorHypothesis : public BehaviorModel {
+  public:
+
+  virtual modules::commons::Probability GetProbability(const Action& action,
+                             const world::ObservedWorld& observed_world,
+                             const AgentId& agent_id) const = 0;
+};
+
+typedef std::shared_ptr<BehaviorHypothesis> BehaviorHypothesisPtr;
+
+
+}  // namespace behavior
+}  // namespace models
+}  // namespace modules
+
+#endif  // MODULES_MODELS_BEHAVIOR_BEHAVIOR_HYPOTHESIS_HPP_
