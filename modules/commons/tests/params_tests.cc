94424bbdfd4a0500430b5c59aca10567e4e1509e
jbernhard <bernhard@fortiss.org>
2020-03-12 09:11:09 +0100

    params get set distribution test

--- modules/commons/tests/params_tests.cc
+++ modules/commons/tests/params_tests.cc
@@ -56,15 +56,15 @@ TEST(setter_params, param_tests) {
 
 
   // Setters used for distribution spec
-  params.SetReal("Test2::25::123::UniformDistribution1D::LowerBound", 10.0f);
+  params.SetReal("Test2::25::123::LowerBound", -10.0f);
   params.SetDistribution("Test2::25::123", "UniformDistribution1D");
   auto dist_uniform = params.GetDistribution("Test2::25::123","some description", "NormalDistribution1D");
-  EXPECT_TRUE(typeid(dist_uniform)== typeid(modules::commons::UniformDistribution1D));
-  EXPECT_EQ(dist_uniform->GetParams()->GetReal("LowerBound", "some description", 2323.0), 10.0f);
+  EXPECT_TRUE(typeid(*dist_uniform)== typeid(modules::commons::UniformDistribution1D));
+  EXPECT_EQ(dist_uniform->GetParams()->GetReal("LowerBound", "some description", 2323.0), -10.0f);
 
   // No defaults for dist spec fiven
   auto dist_uniform2 = params.GetDistribution("Test2::25::123123","some description", "NormalDistribution1D");
-  EXPECT_TRUE(typeid(dist_uniform)== typeid(modules::commons::NormalDistribution1D));
+  EXPECT_TRUE(typeid(*dist_uniform2)== typeid(modules::commons::NormalDistribution1D));
 }
 
 int main(int argc, char **argv) {
