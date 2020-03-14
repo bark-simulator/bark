f8fb01a267c45b6597c3903f54b829db0510a615
jbernhard <bernhard@fortiss.org>
2020-03-14 13:23:26 +0100

    hypothesis test density ratios calculated

--- modules/models/tests/behavior_hypothesis_test.cc
+++ modules/models/tests/behavior_hypothesis_test.cc
@@ -31,13 +31,13 @@ ParamsPtr make_params_hypothesis(float headway_lower, float headway_upper, float
     // Behavior params
     auto params = std::make_shared<SetterParams>(true);
     // IDM Classic
-    params->SetReal("BehaviorIDMClassic::MinimumSpacing", 2.0f);
+    params->SetReal("BehaviorIDMClassic::MinimumSpacing", 0.0f); // Required for testing
     params->SetReal("BehaviorIDMClassic::DesiredTimeHeadway", fixed_headway);
-    params->SetReal("BehaviorIDMClassic::MaxAcceleration", 1.7f);
+    params->SetReal("BehaviorIDMClassic::MaxAcceleration", 1.0f); // Required for testing
     params->SetReal("BehaviorIDMClassic::AccelerationLowerBound", acc_lower_bound);
     params->SetReal("BehaviorIDMClassic::AccelerationUpperBound", acc_upper_bound);
     params->SetReal("BehaviorIDMClassic::DesiredVelocity", 15.0f);
-    params->SetReal("BehaviorIDMClassic::ComfortableBrakingAcceleration",  1.67f);
+    params->SetReal("BehaviorIDMClassic::ComfortableBrakingAcceleration",  1.0f);
     params->SetReal("BehaviorIDMClassic::MinVelocity", 0.0f);
     params->SetReal("BehaviorIDMClassic::MaxVelocity", 50.0f);
     params->SetInt("BehaviorIDMClassic::Exponent", 4);
@@ -111,20 +111,39 @@ TEST(hypothesis_idm_headway, behavior_hypothesis) {
   // Other agent in front, plan action for single parameter (t=1.5 in uniform range (1.0, 3.0))
   // compare to distribution parameter approach, no acceleration limits
   {
-  auto params = make_params_hypothesis(1.0, 3.0, 1.5, -100, 100, -110, 110);
+  auto params = make_params_hypothesis(1.0, 3.0, 1.5, -20, 20, -22, 20);
   auto behavior = BehaviorHypothesisIDMStochasticHeadway(params);
   const float desired_velocity = behavior.GetDesiredVelocity();
 
-  float ego_velocity = desired_velocity+2, rel_distance = 30.0, velocity_difference = 0.0;
+  float ego_velocity = desired_velocity, rel_distance = desired_velocity, velocity_difference = 0.0;
   auto observed_world = make_test_observed_world(
       1, rel_distance, ego_velocity, velocity_difference, goal_definition_ptr);
 
-  auto idm_det_behavior = BehaviorIDMClassic(params);
-  auto traj = idm_det_behavior.Plan(0.2, observed_world);
-  Action action(idm_det_behavior.GetLastAction());
+  // Check only ratios of probabilities, since we do not calculate exact distances with a histogram
+  Action action0(-1.5);
+  Action action1(-2.0);
+  Action action2(-7.5);
   auto ego_agent_id = observed_world.GetAgents().begin()->first;
-  auto action_prob = behavior.GetProbability(action, observed_world, ego_agent_id);
-  EXPECT_NEAR(action_prob, 1/(3.0-1.0), 0.01);
+  auto action_prob0 = behavior.GetProbability(action0, observed_world, ego_agent_id);
+  auto action_prob1 = behavior.GetProbability(action1, observed_world, ego_agent_id);
+  auto action_prob2 = behavior.GetProbability(action2, observed_world, ego_agent_id);
+  auto av0 = boost::get<Continuous1DAction>(action0);
+  auto av1 = boost::get<Continuous1DAction>(action1);
+  auto av2 = boost::get<Continuous1DAction>(action2);
+  // With our param settings: a_idm = t_headway^2 -> f(a_idm) = 1/(4*sqrt(t_headway))
+  Probability desired0  = 1.0/sqrt(4*abs(av0));
+  Probability desired1  = 1.0/sqrt(4*abs(av1));
+  Probability desired2  = 1.0/sqrt(4*abs(av2));
+  EXPECT_NEAR(action_prob1/desired1, action_prob2/desired2, 0.02);
+  EXPECT_NEAR(action_prob1/desired1, action_prob0/desired0, 0.02);
+
+  Action action3(-9.1); //should be zero prob
+  Action action4(-0.9); //should be zero prob
+  auto action_prob3 = behavior.GetProbability(action1, observed_world, ego_agent_id);
+  auto action_prob4 = behavior.GetProbability(action2, observed_world, ego_agent_id);
+  EXPECT_NEAR(action_prob3, 0, 0.01);
+  EXPECT_NEAR(action_prob4, 0, 0.01);
+
   }
 }
 
