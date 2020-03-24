8c03b075792499452af9db6b7d460cb19ca229b8
juloberno <juloberno@gmail.com>
2020-03-24 12:50:01 +0100

    remove action store logging

--- modules/models/behavior/action_store/behavior_action_store.cpp
+++ modules/models/behavior/action_store/behavior_action_store.cpp
@@ -19,8 +19,6 @@ ActionHash BehaviorActionStore::Store(const Action& action, const Trajectory& tr
   auto it = trajectory_store_.find(action_hash);
   if(it == trajectory_store_.end()) {
     trajectory_store_.emplace(std::make_pair(action_hash, trajectory));
-  } else {
-    LOG(WARNING) << "Storing action which is already stored.";
   }
   return action_hash;
 }
