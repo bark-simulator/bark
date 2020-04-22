574a7bc0dae91bd4f143f84b71410666d1165479
Patrick Hart <patrickhart.1990@gmail.com>
2020-04-22 21:07:19 +0200

    extended example

--- examples/three_way_plain.py
+++ examples/three_way_plain.py
@@ -9,7 +9,7 @@ import os
 from modules.runtime.commons.parameters import ParameterServer
 from modules.runtime.viewer.matplotlib_viewer import MPViewer
 from modules.runtime.commons.xodr_parser import XodrParser
-from bark.models.behavior import BehaviorConstantVelocity
+from bark.models.behavior import BehaviorConstantVelocity, BehaviorIDMClassic
 from bark.models.execution import ExecutionModelInterpolate
 from bark.models.dynamic import SingleTrackModel
 from bark.world import World
@@ -27,13 +27,19 @@ param_server = ParameterServer()
 world = World(param_server)
 
 # Model Definitions
-behavior_model = BehaviorConstantVelocity(param_server)
+# behavior_model = BehaviorConstantVelocity(param_server)
+behavior_model = BehaviorIDMClassic(param_server)
 execution_model = ExecutionModelInterpolate(param_server)
 dynamic_model = SingleTrackModel(param_server)
 
-behavior_model2 = BehaviorConstantVelocity(param_server)
+# behavior_model2 = BehaviorConstantVelocity(param_server)
+behavior_model2 = BehaviorIDMClassic(param_server)
 execution_model2 = ExecutionModelInterpolate(param_server)
 dynamic_model2 = SingleTrackModel(param_server)
+
+behavior_model3 = BehaviorIDMClassic(param_server)
+execution_model3 = ExecutionModelInterpolate(param_server)
+dynamic_model3 = SingleTrackModel(param_server)
 
 # Map Definition
 xodr_parser = XodrParser("modules/runtime/tests/data/three_way_plain.xodr")
@@ -43,7 +49,7 @@ world.SetMap(map_interface)
 
 # Agent Definition
 agent_2d_shape = CarLimousine()
-init_state = np.array([0, 2, -20, 3.14/2, 50/3.6])
+init_state = np.array([0, 2, -20, 3.14/2, 43/3.6])
 goal_polygon = Polygon2d([0, 0, 0],[Point2d(-1,-1),Point2d(-1,1),Point2d(1,1), Point2d(1,-1)])
 goal_polygon = goal_polygon.Translate(Point2d(-30, 2))
 agent_params = param_server.addChild("agent1")
@@ -72,6 +78,20 @@ agent2 = Agent(init_state2,
                map_interface)
 world.AddAgent(agent2)
 
+agent_2d_shape3 = CarLimousine()
+init_state3 = np.array([0, -30, -2, -3.14, 50/3.6])
+agent_params3 = param_server.addChild("agent3")
+goal_polygon = Polygon2d([0, 0, 0], [Point2d(-1,-1),Point2d(-1,1),Point2d(1,1), Point2d(1,-1)])
+goal_polygon = goal_polygon.Translate(Point2d(30, -2))
+agent3 = Agent(init_state3,
+               behavior_model3,
+               dynamic_model3,
+               execution_model3,
+               agent_2d_shape3,
+               agent_params3,
+               GoalDefinitionPolygon(goal_polygon),
+               map_interface)
+world.AddAgent(agent3)
 
 # viewer
 viewer = MPViewer(params=param_server, use_world_bounds=True)
