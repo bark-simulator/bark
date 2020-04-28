f18117bbaabdb16d3dbe6342dac41f9cbac784e2
Patrick Hart <patrickhart.1990@gmail.com>
2020-04-28 15:57:36 +0200

    use world bounds in highway example

--- examples/highway.py
+++ examples/highway.py
@@ -22,18 +22,18 @@ from modules.runtime.scenario.scenario_generation.config_with_ease import \
 from modules.runtime.runtime import Runtime
 
 
-# Parameters
+# parameters
 param_server = ParameterServer()
 
-# Scenario
+# scenario
 class CustomLaneCorridorConfig(LaneCorridorConfig):
   def __init__(self,
                road_ids=[16],
                lane_corridor_id=0,
                params=None):
     super(CustomLaneCorridorConfig, self).__init__(road_ids,
-                                                 lane_corridor_id,
-                                                 params)
+                                                   lane_corridor_id,
+                                                   params)
 
   def position(self, world, min_s=10., max_s=150.):
     return super(CustomLaneCorridorConfig, self).position(world, min_s, max_s)
@@ -43,7 +43,6 @@ class CustomLaneCorridorConfig(LaneCorridorConfig):
 
 left_lane = CustomLaneCorridorConfig(lane_corridor_id=0, params=param_server)
 right_lane = CustomLaneCorridorConfig(lane_corridor_id=1, params=param_server)
-
 scenarios = \
   ConfigWithEase(num_scenarios=5,
                  map_file_name="modules/runtime/tests/data/city_highway_straight.xodr",
@@ -51,13 +50,16 @@ scenarios = \
                  params=param_server,
                  lane_corridor_configs=[left_lane, right_lane])
 
-mp_viewer = MPViewer(params=param_server, x_range=[5060, 5160], y_range=[5070,5150])
+# viewer
+mp_viewer = MPViewer(params=param_server,
+                     use_world_bounds=True)
 
 
 # gym like interface
 env = Runtime(step_time=0.2,
               viewer=mp_viewer,
-              scenario_generator=scenarios)
+              scenario_generator=scenarios,
+              render=True)
 env.reset()
 for _ in range(0, 10):
   env.step()
