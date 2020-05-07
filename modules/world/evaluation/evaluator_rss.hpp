d2e17695de3710e0c60c05b646951c8be55ec606
tin1254 <tin-1254@hotmail.com>
2020-05-07 19:36:17 +0200

    add BUILD file to build ad_rss_map_integration

--- modules/world/evaluation/evaluator_rss.hpp
+++ modules/world/evaluation/evaluator_rss.hpp
@@ -3,3 +3,11 @@
 
 #include "spdlog/spdlog.h"
 #include "spdlog/fmt/ostr.h"
+#include <ad/map/landmark/LandmarkIdSet.hpp>
+#include <ad/map/match/Object.hpp>
+#include <ad/map/route/FullRoute.hpp>
+#include <ad/rss/core/RssCheck.hpp>
+#include <ad/rss/map/RssSceneCreation.hpp>
+#include <ad/rss/situation/SituationSnapshot.hpp>
+#include <ad/rss/state/ProperResponse.hpp>
+#include <ad/rss/state/RssStateSnapshot.hpp>
