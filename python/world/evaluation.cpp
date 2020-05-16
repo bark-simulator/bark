68f5b66a1ea614763b6fe4478863cea37569c39d
Karin Frlic <kf5121@student.uni-lj.si>
2020-05-16 20:26:59 +0200

    Revert "Run simulation with different parameters"
    This reverts commit 8eaf9d9991852e1333f5c3f7b546e5fb586903fc.

--- python/world/evaluation.cpp
+++ python/world/evaluation.cpp
@@ -11,7 +11,6 @@
 #include "modules/world/evaluation/evaluator_drivable_area.hpp"
 #include "modules/world/evaluation/evaluator_collision_ego_agent.hpp"
 #include "modules/world/evaluation/evaluator_step_count.hpp"
-#include "modules/world/evaluation/evaluator_distance_to_goal.hpp"
 
 namespace py = pybind11;
 
@@ -71,12 +70,5 @@ void python_evaluation(py::module m) {
     .def(py::init<>())
     .def("__repr__", [](const EvaluatorStepCount &g) {
       return "bark.world.evaluation.EvaluatorStepCount";
-    });
-
-  py::class_<EvaluatorDistanceToGoal, BaseEvaluator,
-    std::shared_ptr<EvaluatorDistanceToGoal> >(m, "EvaluatorDistanceToGoal")
-    .def(py::init<const AgentId&>())
-    .def("__repr__", [](const EvaluatorDistanceToGoal &g) {
-      return "bark.world.evaluation.EvaluatorDistanceToGoal";
     });
 }
