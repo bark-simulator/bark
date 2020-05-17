ebdaa817a93f8bb2c8ca1b185eb29b0e5b808287
tin1254 <tin-1254@hotmail.com>
2020-05-17 19:11:27 +0200

    add basic code in rss evaluator

--- modules/world/evaluation/evaluator_rss.cpp
+++ modules/world/evaluation/evaluator_rss.cpp
@@ -1,4 +1,26 @@
-#include "evaluator_rss.hpp"
+// Copyright (c) 2019 fortiss GmbH, Julian Bernhard, Klemens Esterle, Patrick Hart, Tobias Kessler
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
+
+
+#include "modules/world/evaluation/evaluator_rss.hpp"
+
+namespace modules {
+namespace world {
+class World;
+namespace evaluation {
+
+EvaluationReturn EvaluatorRss::Evaluate(const world::World &world) {
+  bool ego_safety_state = false;
+
+  return ego_safety_state;
+}
+
+}  // namespace evaluation
+}  // namespace world
+}  // namespace modules
+
 
 int main(){
     return 0;
--- modules/world/evaluation/evaluator_rss.hpp
+++ modules/world/evaluation/evaluator_rss.hpp
@@ -1,5 +1,17 @@
+// Copyright (c) 2019 fortiss GmbH, Julian Bernhard, Klemens Esterle, Patrick Hart, Tobias Kessler
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
+
 // #ifndef MODULES_WORLD_EVALUATION_EVALUATOR_RSS_HPP_
 // #define MODULES_WORLD_EVALUATION_EVALUATOR_RSS_HPP_
+
+#include <memory>
+#include <limits>
+
+#include "modules/world/evaluation/base_evaluator.hpp"
+#include "modules/world/world.hpp"
+#include "modules/world/observed_world.hpp"
 
 #include "spdlog/spdlog.h"
 #include "spdlog/fmt/ostr.h"
@@ -11,3 +23,26 @@
 #include <ad/rss/situation/SituationSnapshot.hpp>
 #include <ad/rss/state/ProperResponse.hpp>
 #include <ad/rss/state/RssStateSnapshot.hpp>
+
+namespace modules {
+namespace world {
+namespace evaluation {
+
+class EvaluatorRss : public BaseEvaluator {
+ public:
+  EvaluatorRss() :
+    agent_id_(std::numeric_limits<AgentId>::max()) {}
+  explicit EvaluatorRss(const AgentId& agent_id) :
+    agent_id_(agent_id) {}
+  virtual ~EvaluatorRss() { }
+  virtual EvaluationReturn Evaluate(const world::World& world);
+
+ private:
+  AgentId agent_id_;
+};
+
+}  // namespace evaluation
+}  // namespace world
+}  // namespace modules
+
+// #endif  // MODULES_WORLD_EVALUATION_EVALUATOR_RSS_HPP_
