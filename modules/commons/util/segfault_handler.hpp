842274ec57534cf998beb06d63391e51f714a99e
Patrick <patrickhart.1990@gmail.com>
2020-05-19 13:07:42 +0200

    Integrating segfault handler

---
+++ modules/commons/util/segfault_handler.hpp
@@ -0,0 +1,35 @@
+// Copyright (c) 2019 fortiss GmbH, Julian Bernhard, Klemens Esterle,
+// Patrick Hart, Tobias Kessler
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
+
+
+#ifndef MODULES_COMMONS_SEGFAULT_HANDLER_HPP_
+#define MODULES_COMMONS_SEGFAULT_HANDLER_HPP_
+
+#include <stdio.h>
+#include <execinfo.h>
+#include <csignal>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+namespace modules {
+namespace commons {
+
+void SegfaultHandler(int sig) {
+  void *array[20];
+  size_t size;
+  size = backtrace(array, 10);
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
+
+
+}  // namespace commons
+}  // namespace modules
+
+
+#endif  // MODULES_COMMONS_SEGFAULT_HANDLER_HPP_
