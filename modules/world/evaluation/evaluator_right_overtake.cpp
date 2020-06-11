e60f87101410a343a7bc06af2db91f42fa9beef1
cirrostratus1 <ge69xek@tum.de>
2020-06-11 07:09:12 +0200

    Use DenseTrafficLabelFunction

--- modules/world/evaluation/evaluator_right_overtake.cpp
+++ modules/world/evaluation/evaluator_right_overtake.cpp
@@ -6,6 +6,7 @@
 #include "modules/world/evaluation/evaluator_right_overtake.hpp"
 
 #include "modules/world/evaluation/labels/behind_of_label_function.hpp"
+#include "modules/world/evaluation/labels/dense_traffic_label_function.hpp"
 #include "modules/world/evaluation/labels/front_of_label_function.hpp"
 #include "modules/world/evaluation/labels/right_of_label_function.hpp"
 
@@ -13,9 +14,10 @@ namespace modules {
 namespace world {
 namespace evaluation {
 const char EvaluatorRightOvertake::formula_[] =
-    "G !(b_v#0 & X[!](b_v#0 U r_v#0 U f_v#0))";
+    "G (dense -> !(b_v#0 & X[!](b_v#0 U r_v#0 U f_v#0)))";
 
 const LabelFunctions EvaluatorRightOvertake::labels_ = {
+    LabelFunctionPtr(new DenseTrafficLabelFunction("dense", 20.0, 8)),
     LabelFunctionPtr(new RightOfLabelFunction("r_v")),
     LabelFunctionPtr(new FrontOfLabelFunction("f_v")),
     LabelFunctionPtr(new BehindOfLabelFunction("b_v"))};
