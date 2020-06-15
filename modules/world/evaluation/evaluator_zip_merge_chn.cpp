8e6f7931ddafccabeb36682028455557c06a7dae
cirrostratus1 <ge69xek@tum.de>
2020-06-15 09:48:47 +0200

    Fix EvaluatorZipMergeChn

--- modules/world/evaluation/evaluator_zip_merge_chn.cpp
+++ modules/world/evaluation/evaluator_zip_merge_chn.cpp
@@ -17,22 +17,22 @@ namespace modules {
 namespace world {
 namespace evaluation {
 const char EvaluatorZipMergeChn::formula_[] =
-    "(F(i_left_of_k#1 & i_behind_of_k#1 & k_near_i#1 & k_near_lane_end#1) & "
+    "G (((i_left_of_k#1 & i_behind_of_k#1 & k_near_i#1 & k_near_lane_end#1) & "
     "j_precedes_i#0 & !merged_i & near_merge_i & (j_precedes_i#0 | merged_j#0) U "
-    "merged_i) -> G(merged_i & merged_j#0 -> !j_precedes_i#0)";
+    "merged_i) -> G(merged_i & merged_j#0 -> !j_precedes_i#0))";
 
 const LabelFunctions EvaluatorZipMergeChn::labels_ = {
     LabelFunctionPtr(
-        new EgoBeyondPointLabelFunction("near_merge_i", Point2d(-55.0, -164.0))),
+        new EgoBeyondPointLabelFunction("near_merge_i", Point2d(1052.0, 943.0))),
     LabelFunctionPtr(
-        new AgentBeyondPointLabelFunction("merged_j", Point2d(-22.0, -168.0))),
+        new AgentBeyondPointLabelFunction("merged_j", Point2d(1104.0, 943.0))),
     LabelFunctionPtr(
-        new EgoBeyondPointLabelFunction("merged_i", Point2d(-22.0, -168.0))),
+        new EgoBeyondPointLabelFunction("merged_i", Point2d(1104.0, 943.0))),
     LabelFunctionPtr(new PrecedingAgentLabelFunction("j_precedes_i")),
     LabelFunctionPtr(new LeftOfLabelFunction("i_left_of_k")),
     LabelFunctionPtr(new BehindOfLabelFunction("i_behind_of_k")),
     LabelFunctionPtr(new AgentNearLabelFunction("k_near_i", 6.0)),
-    LabelFunctionPtr(new AgentAtLaneEndLabelFunction("k_near_lane_end", 55.0))};
+    LabelFunctionPtr(new AgentAtLaneEndLabelFunction("k_near_lane_end", 20.0))};
 
 }  // namespace evaluation
 }  // namespace world
