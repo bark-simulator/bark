b38f1e4b09f972ca4fc208be9d6185b0b3bbec26
juloberno <juloberno@gmail.com>
2020-06-29 13:17:35 +0200

    correct behavior model config readers

--- bark/runtime/scenario/scenario_generation/config_readers/behavior_model_config_readers.py
+++ bark/runtime/scenario/scenario_generation/config_readers/behavior_model_config_readers.py
@@ -1,20 +1,21 @@
-# Copyright (c) 2020 Julian Bernhard, Klemens Esterle, Patrick Hart and
-# Tobias Kessler
+# Copyright (c) 2020 fortiss GmbH
 #
-# This work is licensed under the terms of the MIT license.
-# For a copy, see <https://opensource.org/licenses/MIT>.
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
 
-
+import numpy as np
 
 from bark.runtime.scenario.scenario_generation.config_readers.config_readers_interfaces import ConfigReaderBehaviorModels
+from bark.models.behavior.hypothesis.behavior_space.behavior_space import BehaviorSpace
+from bark.runtime.scenario.scenario_generation.interaction_dataset_reader import behavior_from_track
 
 from bark.core.models.behavior import *
 from bark.runtime.commons.parameters import ParameterServer
 
   # this config reader defines behavior models with fixed type for all agents
 class FixedBehaviorType(ConfigReaderBehaviorModels):
-  def __init__(self, random_state):
-    super().__init__(random_state)
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
     self.param_servers = []
 
   def create_from_config(self, config_param_object, road_corridor, agent_states,  **kwargs):
@@ -24,24 +25,75 @@ class FixedBehaviorType(ConfigReaderBehaviorModels):
     self.param_servers.append(model_params) # use the same param server for all models
     behavior_models = []
     behavior_model_types = []
-    model_default_params = ParameterServer(json={})
     for _ in agent_states:
-      bark_model, params = self.model_from_model_type(model_type, model_params)
+      bark_model, _ = self.model_from_model_type(model_type, model_params)
       behavior_models.append(bark_model)
       behavior_model_types.append(model_type)
     return behavior_models, {"behavior_model_types" : behavior_model_types}, config_param_object
 
   def model_from_model_type(self, model_type, params):
-    bark_model = eval("{}(params)".format(model_type))    
+    bark_model = eval("{}(params)".format(model_type))
     return bark_model, params
 
   def get_param_servers(self):
     return self.param_servers
 
 
+class BehaviorSpaceSampling(ConfigReaderBehaviorModels):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.param_servers = []
+
+  def create_from_config(self, config_param_object, road_corridor, agent_states,  **kwargs):
+    behavior_space = BehaviorSpace(config_param_object)
+    # now do the true sampling
+    behavior_models = []
+    behavior_model_types = []
+
+    for _ in agent_states:
+      model_params_sampled, model_type = behavior_space.sample_behavior_parameters(self.random_state)
+      self.param_servers.append(model_params_sampled)
+      bark_model, _ = self.model_from_model_type(model_type, model_params_sampled)
+      behavior_models.append(bark_model)
+      behavior_model_types.append(model_type)
+    return behavior_models, {"behavior_model_types" : behavior_model_types}, config_param_object
+
+  def model_from_model_type(self, model_type, params):
+    bark_model = eval("{}(params)".format(model_type))
+    return bark_model, params
+
+  def get_param_servers(self):
+    return self.param_servers
+
+
+class InteractionDataBehaviors(ConfigReaderBehaviorModels):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.param_servers = []
+
+  def create_from_config(self, config_param_object, road_corridor, agent_states,  **kwargs):
+    tracks = kwargs["tracks"]
+    start_time = kwargs["start_time"]
+    end_time = kwargs["end_time"]
+
+    behavior_models = []
+    behavior_model_types = []
+
+    for idx, _ in enumerate(agent_states):
+      track = tracks[idx]
+      params = ParameterServer()
+      behavior = behavior_from_track(track, params, start_time, end_time)
+      self.param_servers.append(params)
+      behavior_models.append(behavior)
+      behavior_model_types.append("BehaviorStaticTrajectory")
+    return behavior_models, {"behavior_model_types" : behavior_model_types}, config_param_object
+
+  def get_param_servers(self):
+    return self.param_servers
+
 class SampleBehaviorType(ConfigReaderBehaviorModels):
-  def __init__(self, random_state):
-    super().__init__(random_state)
+  def __init__(self, *args, **kwargs):
+    super().__init__( *args, **kwargs)
     self.param_servers = []
 
   def create_from_config(self, config_param_object, road_corridor, agent_states,  **kwargs):
