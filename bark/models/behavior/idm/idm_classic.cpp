63c6175f05d5bd08168509cdb6f2afda6086d98d
jbernhard <bernhard@fortiss.org>
2020-06-30 16:36:35 +0200

    idm classic maintain first acceleration

--- bark/models/behavior/idm/idm_classic.cpp
+++ bark/models/behavior/idm/idm_classic.cpp
@@ -10,8 +10,8 @@
 #include <cmath>
 #include <limits>
 #include <memory>
-#include <utility>
 #include <tuple>
+#include <utility>
 
 #include "bark/commons/transformation/frenet.hpp"
 #include "bark/world/observed_world.hpp"
@@ -29,12 +29,10 @@ using bark::world::objects::AgentPtr;
 using bark::world::map::LaneCorridor;
 using bark::world::map::LaneCorridorPtr;
 
-
 std::tuple<Trajectory, Action> BehaviorIDMClassic::GenerateTrajectory(
-  const world::ObservedWorld& observed_world,
-  const LaneCorridorPtr& lane_corr,
-  const IDMRelativeValues& rel_values,
-  double dt) const {
+    const world::ObservedWorld &observed_world,
+    const LaneCorridorPtr &lane_corr, const IDMRelativeValues &rel_values,
+    double dt) const {
   double t_i = 0., acc = 0.;
   geometry::Line line = lane_corr->GetCenterLine();
   dynamic::Trajectory traj(GetNumTrajectoryTimePoints(),
@@ -42,13 +40,14 @@ std::tuple<Trajectory, Action> BehaviorIDMClassic::GenerateTrajectory(
   dynamic::State ego_vehicle_state = observed_world.CurrentEgoState();
   geometry::Point2d pose = observed_world.CurrentEgoPosition();
 
+  double initial_acceleration = 0.0f;
   if (!line.obj_.empty()) {
     // adding state at t=0
     traj.block<1, StateDefinition::MIN_STATE_SIZE>(0, 0) =
-      ego_vehicle_state.transpose().block<1, StateDefinition::MIN_STATE_SIZE>(
-        0, 0);
+        ego_vehicle_state.transpose().block<1, StateDefinition::MIN_STATE_SIZE>(
+            0, 0);
 
-    float s_start = GetNearestS(line, pose);  // checked
+    float s_start = GetNearestS(line, pose); // checked
     double start_time = observed_world.GetWorldTime();
     float vel_i = ego_vehicle_state(StateDefinition::VEL_POSITION);
     float s_i = s_start;
@@ -57,12 +56,16 @@ std::tuple<Trajectory, Action> BehaviorIDMClassic::GenerateTrajectory(
     // calc. traj.
     for (int i = 1; i < GetNumTrajectoryTimePoints(); ++i) {
       std::tie(acc, rel_distance) =
-        GetTotalAcc(observed_world, rel_values, rel_distance, dt);
+          GetTotalAcc(observed_world, rel_values, rel_distance, dt);
       BARK_EXPECT_TRUE(!std::isnan(acc));
+      // Set initial acceleration to maintain action value
+      if (i == 1) {
+        initial_acceleration = acc;
+      }
       s_i += 0.5f * acc * dt * dt + vel_i * dt;
       const float temp_velocity = vel_i + acc * dt;
-      vel_i = std::max(std::min(
-        temp_velocity, GetMaxVelocity()), GetMinVelocity());
+      vel_i =
+          std::max(std::min(temp_velocity, GetMaxVelocity()), GetMinVelocity());
       t_i = static_cast<float>(i) * dt + start_time;
       geometry::Point2d traj_point = GetPointAtS(line, s_i);
       float traj_angle = GetTangentAngleAtS(line, s_i);
@@ -73,18 +76,18 @@ std::tuple<Trajectory, Action> BehaviorIDMClassic::GenerateTrajectory(
 
       traj(i, StateDefinition::TIME_POSITION) = t_i;
       traj(i, StateDefinition::X_POSITION) =
-        boost::geometry::get<0>(traj_point);
+          boost::geometry::get<0>(traj_point);
       traj(i, StateDefinition::Y_POSITION) =
-        boost::geometry::get<1>(traj_point);
+          boost::geometry::get<1>(traj_point);
       traj(i, StateDefinition::THETA_POSITION) = traj_angle;
       traj(i, StateDefinition::VEL_POSITION) = vel_i;
     }
   }
 
-  Action action(acc);
+  Action action(initial_acceleration);
   return std::tuple<Trajectory, Action>(traj, action);
 }
 
-}  // namespace behavior
-}  // namespace models
-}  // namespace bark
+} // namespace behavior
+} // namespace models
+} // namespace bark
