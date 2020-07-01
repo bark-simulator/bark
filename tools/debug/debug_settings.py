db71de77b0276e17d8847c50966e9967fc909ca5
juloberno <juloberno@gmail.com>
2020-07-01 12:53:50 +0200

    add python debug settings build file

--- tools/debug/debug_settings.py
+++ tools/debug/debug_settings.py
@@ -1,14 +1,16 @@
 import sys
 import os
 
+
+
 cwd = os.getcwd()
 workspace_folder = cwd
 repo_paths = ["bark_project", "bark_project/python", "benchmark_database", "com_github_interaction_dataset_interaction_dataset", \
         "com_github_interaction_dataset_interaction_dataset/python", "phd"]
 
 executed_file = sys.argv[0]
-tmp = executed_file.replace("bark", "bazel-bin/bark")
-tmp = tmp.replace("python/", "bazel-bin/python/")
+tmp = "bazel-bin/bark".join(executed_file.rsplit("bark",1))
+tmp = tmp.replace("python_wrapper/", "bazel-bin/python_wrapper/")
 runfiles_dir = tmp.replace(".py", ".runfiles")
 
 sys.path.append(runfiles_dir)
