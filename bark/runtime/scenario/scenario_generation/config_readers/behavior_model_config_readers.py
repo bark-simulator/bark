60bed1ccd10b7992ca964a55b3bc18290520987e
juloberno <juloberno@gmail.com>
2020-07-03 11:45:25 +0200

    move behavior space to other repo

--- bark/runtime/scenario/scenario_generation/config_readers/behavior_model_config_readers.py
+++ bark/runtime/scenario/scenario_generation/config_readers/behavior_model_config_readers.py
@@ -42,33 +42,6 @@ class FixedBehaviorType(ConfigReaderBehaviorModels):
     return self.param_servers
 
 
-class BehaviorSpaceSampling(ConfigReaderBehaviorModels):
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.param_servers = []
-
-  def create_from_config(self, config_param_object, road_corridor, agent_states,  **kwargs):
-    behavior_space = BehaviorSpace(config_param_object)
-    # now do the true sampling
-    behavior_models = []
-    behavior_model_types = []
-
-    for _ in agent_states:
-      model_params_sampled, model_type = behavior_space.sample_behavior_parameters(self.random_state)
-      self.param_servers.append(model_params_sampled)
-      bark_model, _ = self.model_from_model_type(model_type, model_params_sampled)
-      behavior_models.append(bark_model)
-      behavior_model_types.append(model_type)
-    return behavior_models, {"behavior_model_types" : behavior_model_types}, config_param_object
-
-  def model_from_model_type(self, model_type, params):
-    bark_model = eval("{}(params)".format(model_type))
-    return bark_model, params
-
-  def get_param_servers(self):
-    return self.param_servers
-
-
 class InteractionDataBehaviors(ConfigReaderBehaviorModels):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
