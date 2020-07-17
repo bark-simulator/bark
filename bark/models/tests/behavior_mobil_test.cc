4bf576c4057c9899b822d280e2b0cb618905a75a
klemense1 <esterle@fortiss.org>
2020-07-17 15:54:26 +0200

    adding test case no_lane_change_to_ending_lane

--- bark/models/tests/behavior_mobil_test.cc
+++ bark/models/tests/behavior_mobil_test.cc
@@ -225,7 +225,7 @@ TEST(polite_incentive_met_safety_met, behavior_mobil) {
   BARK_EXPECT_TRUE(lane_corr != nullptr);
 }
 
-TEST(lane_ending, behavior_mobil) {
+TEST(change_lane_due_to_lane_ending, behavior_mobil) {
 
   double vel_ego = 5.0;
   auto params = std::make_shared<SetterParams>();
@@ -283,6 +283,78 @@ TEST(lane_ending, behavior_mobil) {
   EXPECT_EQ(decision, LaneChangeDecision::ChangeLane);
   BARK_EXPECT_TRUE(lane_corr != nullptr);
 }
+
+TEST(no_lane_change_to_ending_lane, behavior_mobil) {
+
+  double vel = 5.0;
+  auto params = std::make_shared<SetterParams>();
+  params->SetReal("BehaviorIDMClassic::DesiredVelocity", vel);
+  params->SetBool("BehaviorIDMClassic::BrakeForLaneEnd", true);
+  params->SetReal("BehaviorIDMClassic::BrakeForLaneEndEnabledDistance", 60);
+  params->SetReal("BehaviorIDMClassic::MaxAcceleration",  1.9);
+  params->SetReal("BehaviorMobilRuleBased::AThr", 0.1);
+  params->SetReal("BehaviorMobilRuleBased::Politeness", 0.5);
+  params->SetReal("BehaviorMobilRuleBased::BSafe", 4.0);
+
+  // Setting Up Map
+  auto open_drive_map = MakeXodrMapEndingLaneInParallel();
+  auto map_interface = std::make_shared<MapInterface>();
+  map_interface->interface_from_opendrive(open_drive_map);
+
+  Polygon car_polygon = CarRectangle();
+  
+  Polygon polygon = GoalRectangle();
+  std::shared_ptr<Polygon> goal_polygon(
+      std::dynamic_pointer_cast<Polygon>(polygon.Translate(
+          Point2d(100, -2))));  // < move the goal polygon into the driving
+                               // corridor in front of the ego vehicle
+  auto goal_definition_ptr =
+      std::make_shared<GoalDefinitionPolygon>(*goal_polygon);
+
+  ExecutionModelPtr exec_model(new ExecutionModelInterpolate(params));
+  DynamicModelPtr dyn_model(new SingleTrackModel(params));
+  BehaviorModelPtr beh_model(new BehaviorMobilRuleBased(params));
+
+  State init_state1(static_cast<int>(StateDefinition::MIN_STATE_SIZE));
+  init_state1 << 0.0, 30.0, -1.75, 0.0, vel;
+  AgentPtr agent1(new Agent(init_state1, beh_model, dyn_model, exec_model,
+                            car_polygon, params, goal_definition_ptr,
+                            map_interface, bark::geometry::Model3D()));
+
+  // Preceding Agent
+  ExecutionModelPtr exec_model2(new ExecutionModelInterpolate(params));
+  DynamicModelPtr dyn_model2(new SingleTrackModel(params));
+  BehaviorModelPtr beh_model2(new BehaviorConstantVelocity(params));
+
+  State init_state2(static_cast<int>(StateDefinition::MIN_STATE_SIZE));
+  init_state2 << 0.0, 60.0, -1.75, 0.0, vel;
+  AgentPtr agent2(new Agent(init_state2, beh_model2, dyn_model2, exec_model2,
+                            car_polygon, params, goal_definition_ptr,
+                            map_interface, bark::geometry::Model3D()));
+  
+  // Construct World
+  WorldPtr world(new World(params));
+  world->AddAgent(agent1);
+  world->AddAgent(agent2);
+  world->UpdateAgentRTree();
+
+  WorldPtr current_world_state(world->Clone());
+  ObservedWorld observed_world(current_world_state, agent1->GetAgentId());
+
+  const BehaviorModelPtr behavior_model = agent1->GetBehaviorModel();
+
+  auto behavior_mobil =
+      std::dynamic_pointer_cast<BehaviorMobilRuleBased>(behavior_model);
+  behavior_mobil->SetLaneCorridor(observed_world.GetLaneCorridor());
+
+  LaneChangeDecision decision;
+  LaneCorridorPtr lane_corr;
+  std::tie(decision, lane_corr) =
+      behavior_mobil->CheckIfLaneChangeBeneficial(observed_world);
+  EXPECT_EQ(decision, LaneChangeDecision::KeepLane);
+  BARK_EXPECT_TRUE(lane_corr != nullptr);
+}
+
 
 
 
