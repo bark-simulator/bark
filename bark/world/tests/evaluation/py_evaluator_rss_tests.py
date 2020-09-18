6fb713b4885519ca1b4f83fb85531f098b1bbd58
tin1254 <tin-1254@hotmail.com>
2020-09-18 02:23:32 +0200

    add copyright comments in rss files

--- bark/world/tests/evaluation/py_evaluator_rss_tests.py
+++ bark/world/tests/evaluation/py_evaluator_rss_tests.py
@@ -1,3 +1,11 @@
+# Copyright (c) 2020 fortiss GmbH
+#
+# Authors: Julian Bernhard, Klemens Esterle, Patrick Hart and
+# Tobias Kessler
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+
 import unittest
 import numpy as np
 
