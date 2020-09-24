cd45f2b5a382b014577363557ea1529fa2c50493
mansoorcheema <mansoorcheema1@gmail.com>
2020-09-25 01:51:58 +0200

    added manylinux dockerfile

---
+++ manylinux_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker build -t mansoorcheema/bazel-manylinux -f tools/ManyLinuxDockerfile .
+docker run -it -v $(pwd):/io mansoorcheema/bazel-manylinux:latest /bin/bash
+cd /io
+virtualenv --python=/opt/python/cp37-cp37m/bin/python ./bark/python_wrapper/venv
+source ./bark/python_wrapper/venv/bin/activate && pip install -r tools/installers/requirements.txt
+bash package.sh manylinux
