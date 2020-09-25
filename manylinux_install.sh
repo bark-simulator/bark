f239ad7388031dec21363c36802dea3e3c650f8c
mansoorcheema <mansoorcheema1@gmail.com>
2020-09-25 02:33:50 +0200

    added manylinus workflow file

--- manylinux_install.sh
+++ manylinux_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker build -t mansoorcheema/bazel-manylinux -f tools/ManyLinuxDockerfile .
-docker run -it -v $(pwd):/io mansoorcheema/bazel-manylinux:latest /bin/bash
-cd /io
+#docker build -t mansoorcheema/bazel-manylinux -f tools/ManyLinuxDockerfile .
+#docker run -it -v $(pwd):/io mansoorcheema/bazel-manylinux:latest /bin/bash
+cd /bark
 virtualenv --python=/opt/python/cp37-cp37m/bin/python ./bark/python_wrapper/venv
 source ./bark/python_wrapper/venv/bin/activate && pip install -r tools/installers/requirements.txt
 bash package.sh manylinux
