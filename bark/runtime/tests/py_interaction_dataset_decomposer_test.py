bc8c313b9da2e280733cdb5b11fd27d476aef48c
klemense1 <esterle@fortiss.org>
2020-10-29 17:16:34 +0100

    implementing xy offset for interaction dataset

--- bark/runtime/tests/py_interaction_dataset_decomposer_test.py
+++ bark/runtime/tests/py_interaction_dataset_decomposer_test.py
@@ -16,16 +16,29 @@ class DatasetDecomposerTest(unittest.TestCase):
 
         map_filename = os.path.join(os.path.dirname(__file__),"data/DR_DEU_Merging_MT_v01_shifted.xodr")
         track_filename = os.path.join(os.path.dirname(__file__),"data/interaction_dataset_dummy_track.csv")
+        xy_offset = [0, 0]
 
         dataset_decomposer = DatasetDecomposer(
-            map_filename=map_filename, track_filename=track_filename)
+            map_filename=map_filename, track_filename=track_filename, xy_offset=xy_offset)
 
         scenario_list = dataset_decomposer.decompose()
         for scen in scenario_list:
           self.assertEqual(int(scen._start_ts), scen._start_ts)
           self.assertEqual(int(scen._end_ts), scen._end_ts)
 
+    def test_decompose_dataset_centered(self):
 
+        map_filename = os.path.join(os.path.dirname(__file__),"data/DR_DEU_Merging_MT_v01_centered.xodr")
+        track_filename = os.path.join(os.path.dirname(__file__),"data/interaction_dataset_dummy_track.csv")
+        xy_offset = [-900, -900]
+
+        dataset_decomposer = DatasetDecomposer(
+            map_filename=map_filename, track_filename=track_filename, xy_offset=xy_offset)
+
+        scenario_list = dataset_decomposer.decompose()
+        for scen in scenario_list:
+          self.assertEqual(int(scen._start_ts), scen._start_ts)
+          self.assertEqual(int(scen._end_ts), scen._end_ts)
 
 if __name__ == '__main__':
     unittest.main()
--- bark/runtime/tests/py_interaction_dataset_reader_test.py
+++ bark/runtime/tests/py_interaction_dataset_reader_test.py
@@ -30,7 +30,7 @@ class InteractionDatasetReaderTest(unittest.TestCase):
         print(agent2)
 
         scen_info = ScenarioTrackInfo(
-            map_filename, track_filename, agent1, start_ts=500, end_ts=1000)
+            map_filename, track_filename, agent1, xy_offset = [0, 0], start_ts=500, end_ts=1000)
         scen_info.AddTrackInfoOtherAgent(agent2)
 
         print(scen_info)
@@ -47,10 +47,8 @@ class InteractionDatasetReaderTest(unittest.TestCase):
 
         params = ParameterServer()
         params_id = params["Scenario"]["Generation"]["InteractionDataset"]
-        params_id["MapFilename", "",
-                  os.path.join(os.path.dirname(__file__),"../tests/data/DR_DEU_Merging_MT_v01_shifted.xodr")]
-        params_id["TrackFilename", "",
-                  os.path.join(os.path.dirname(__file__),"../tests/data/interaction_dataset_dummy_track.csv")]
+        params_id["MapFilename", "", map_filename]
+        params_id["TrackFilename", "", track_filename]
         params_id["BehaviorModel", "", {}]
 
         track_params = ParameterServer()
@@ -60,7 +58,34 @@ class InteractionDatasetReaderTest(unittest.TestCase):
         track_params["behavior_model"] = None
 
         scenario_info = ScenarioTrackInfo(
-            map_filename, track_filename, agent_track_info)
+            map_filename, track_filename, agent_track_info, xy_offset=[0, 0])
+        ds_reader = InteractionDatasetReader()
+        agent = ds_reader.AgentFromTrackfile(
+            track_params, params, scenario_info, agent_track_info.GetTrackId())
+
+
+    def test_agent_from_trackfile_centered(self):
+
+        map_filename = os.path.join(os.path.dirname(__file__),"../tests/data/DR_DEU_Merging_MT_v01_centered.xodr")
+        track_filename = os.path.join(os.path.dirname(__file__),"../tests/data/interaction_dataset_dummy_track.csv")
+
+        agent_track_info = AgentTrackInfo(
+            track_filename, track_id=1, start_offset=500, end_offset=1000)
+
+        params = ParameterServer()
+        params_id = params["Scenario"]["Generation"]["InteractionDataset"]
+        params_id["MapFilename", "", map_filename]
+        params_id["TrackFilename", "", track_filename]
+        params_id["BehaviorModel", "", {}]
+
+        track_params = ParameterServer()
+        track_params["execution_model"] = 'ExecutionModelInterpolate'
+        track_params["dynamic_model"] = 'SingleTrackModel'
+        track_params["map_interface"] = None  # world.map
+        track_params["behavior_model"] = None
+
+        scenario_info = ScenarioTrackInfo(
+            map_filename, track_filename, agent_track_info, xy_offset=[-900, -900])
         ds_reader = InteractionDatasetReader()
         agent = ds_reader.AgentFromTrackfile(
             track_params, params, scenario_info, agent_track_info.GetTrackId())
