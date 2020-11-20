0c95ec46d1807873b7cf64e87ba2267a50b0b4c4
klemense1 <esterle@fortiss.org>
2020-11-20 15:45:15 +0100

    bugfix idm to stay within long acc limits

--- bark/models/behavior/idm/base_idm.cpp
+++ bark/models/behavior/idm/base_idm.cpp
@@ -75,6 +75,7 @@ BaseIDM::BaseIDM(const commons::ParamsPtr& params) : BehaviorModel(params) {
   acceleration_limits_.lon_acc_min =
       params->GetReal("BehaviorIDMClassic::AccelerationLowerBound",
                       "Minimum longitudinal acceleration", -5.0);
+  std::cout << "Constructor " << acceleration_limits_ << std::endl;
 
   SetLastAction(Continuous1DAction(0.0f));
 }
@@ -329,7 +330,11 @@ std::pair<double, double> BaseIDM::GetTotalAcc(
     traveled_other = vel_front * dt;
     rel_distance += traveled_other - traveled_ego;
   } else {
+    const double acc_lower_bound = GetAccelerationLimits().lon_acc_min;
+    const double acc_upper_bound = GetAccelerationLimits().lon_acc_max;
     acc = GetLonAccelerationMax() * CalcFreeRoadTerm(vel_i);
+    acc = std::max(std::min(acc, acc_upper_bound), acc_lower_bound);
+    std::cout << "!!!!!!!!!Set acceleration to " << acc << GetAccelerationLimits() << std::endl;
   }
   return std::pair<double, double>(acc, rel_distance);
 }
