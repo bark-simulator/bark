27ea55ece902267c7ee497f93ae8095895850561
patrickhart <patrickhart.1990@gmail.com>
2021-05-26 14:42:25 +0200

    Setup python build and CI

--- setup.py
+++ setup.py
@@ -1,24 +1,109 @@
 from setuptools import setup, find_packages, Extension
 import os,sys
+import os
+import shlex
+import shutil
+import setuptools.command.build_ext
+import setuptools.command.build_py
+import setuptools.command.install
+import setuptools.command.sdist
+import setuptools.dist
+from setuptools import dist
+from setuptools.command.install import install
+from distutils.command.build import build
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-    
 
-# A dummy native extension to mark module as platform specific
-ext_modules= []
+
+class CustomBuild(build):
+  def run(self):
+    self.build_lib = '_build'
+
 try:
-    os.mkdir('build')
-except FileExistsError:
-    # directory already exists - is already created by earlier run
-    pass
-open('build/temp.c','w').close()
-temp_ext = Extension('_temp', sources=['build/temp.c'])
-ext_modules.append(temp_ext)
+  from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+  class bdist_wheel(_bdist_wheel):
+    def finalize_options(self):
+      _bdist_wheel.finalize_options(self)
+      self.root_is_pure = False
+except ImportError:
+  bdist_wheel = None
+
+class BinaryDistribution(dist.Distribution):
+  def is_pure(self):
+    return False
+  def has_ext_modules(self):
+    return True
+
+class InstallPlatlib(install):
+  def finalize_options(self):
+    install.finalize_options(self)
+    if self.distribution.has_ext_modules():
+      self.install_lib = self.install_platlib
+
+class BuildExtCommand(setuptools.command.build_ext.build_ext):
+  """Overrides default build_ext command to invoke bazel."""
+
+  def run(self):
+    if not self.dry_run:
+      prebuilt_path = os.getenv('BARK_PREBUILT_DIR')
+      if not prebuilt_path:
+        # Ensure python_configure.bzl finds the correct Python verison.
+        os.environ['PYTHON_BIN_PATH'] = sys.executable
+        bazelisk = os.getenv('BARK_BAZELISK', 'bazelisk.py')
+        # Controlled via `setup.py build_ext --debug` flag.
+        default_compilation_mode = 'dbg' if self.debug else 'opt'
+        compilation_mode = os.getenv('BARK_BAZEL_COMPILATION_MODE',
+                                     default_compilation_mode)
+        build_command = [sys.executable, '-u', bazelisk] + [
+            'build',
+            '-c',
+            compilation_mode,
+            '//bark:pip_package',
+            '--verbose_failures'
+        ]
+        if 'darwin' in sys.platform:
+          # Note: Bazel does not use the MACOSX_DEPLOYMENT_TARGET environment
+          # variable.
+          # build_command += ['--macos_minimum_os=%s' % _macos_deployment_target]
+          pass
+        if sys.platform == 'win32':
+          # Disable newer exception handling from Visual Studio 2019, since it
+          # requires a newer C++ runtime than shipped with Python.
+          #
+          # https://cibuildwheel.readthedocs.io/en/stable/faq/#importerror-dll-load-failed-the-specific-module-could-not-be-found-error-on-windows
+          build_command += ['--copt=/d2FH4-']
+
+        self.spawn(build_command)
+        suffix = '.pyd' if os.name == 'nt' else '.so'
+        built_ext_path = os.path.join(
+          'bazel-bin/bark/pip_package.runfiles/bark_project/bark/core' + suffix)
+
+      # TODO: what does this do
+      # os.makedirs(os.path.dirname(ext_full_path), exist_ok=True)
+      copy_to = os.path.dirname(os.path.abspath(__file__)) + "/bark/core.so"
+      copy_to_manifest = os.path.dirname(os.path.abspath(__file__)) + "/MANIFEST.in"
+      print('Copying extension %s -> %s' % (
+          built_ext_path,
+          copy_to
+      ))
+      shutil.copyfile(built_ext_path, copy_to)
+
+      # new_lines = []
+      # with open('bazel-bin/bark/pip_package.runfiles/MANIFEST', 'r') as reader:
+      #   line = reader.readline()
+      #   while line != '':
+      #     line = reader.readline()
+      #     line = line.replace("bark_project/", "")
+      #     new_lines.append(line)
+
+      # with open(copy_to_manifest, 'w') as fo:
+      #   for d in new_lines:
+      #     fo.write(str(d))
 
 setup(
     name = "bark-simulator",
-    version = "1.0.1",
+    version = "1.0.2",
     description = "A tool for Behavior benchmARKing",
     long_description_content_type="text/markdown",
     long_description=long_description,
@@ -51,11 +136,17 @@ setup(
       "jupyter>=1.0.0",
       "ipython>=7.13.0"
     ],
-    ext_modules=ext_modules,
+    cmdclass={
+      'bdist_wheel': bdist_wheel,
+      'build_ext': BuildExtCommand,
+      'install': InstallPlatlib,
+      'build': CustomBuild
+    },
     test_suite='nose.collector',
     tests_require=['nose'],
     include_package_data=True,
     zip_safe=False,
-    python_requires='>=3.7',
+    distclass=BinaryDistribution,
+    python_requires='>=3.6',
 )
 
--- tools/Readme.md
+++ tools/Readme.md
@@ -1,6 +1,15 @@
+# Docker deploy
+
 ## Login into Dockerhub
+
 `docker login`
+
 ## Build & Deploy
+
 Go in the `/tools` folder and run:
 `docker build -t barksim/bark .`
 `docker push barksim/bark:latest`
+
+Or for manylinux:
+`docker build -t barksim/bazel-manylinux .`
+`docker push barksim/bazel-manylinux:latest`
