8ddbb7f83812aae9206d704b43410774540dd109
juloberno <juloberno@gmail.com>
2022-01-05 10:14:49 +0100

    switch to idm classic with take and give way parameters

--- bark/examples/intersection.py
+++ bark/examples/intersection.py
@@ -18,66 +18,74 @@ from bark.examples.paths import Data
 
 
 # Parameters Definitions
-param_server = ParameterServer()
+param_server_give_way = ParameterServer()
+param_server_take_way = ParameterServer()
 
-param_server["BehaviorIDMLaneTracking"]["CrosstrackErrorGain"] = 2.5
-param_server["BehaviorIDMClassic"]["DesiredVelocity"] = 5.
-param_server["BehaviorIntersectionRuleBased"]["BrakingDistance"] = 10.
-param_server["BehaviorIntersectionRuleBased"]["PredictionTimeHorizon"] = 5.
-param_server["BehaviorIntersectionRuleBased"]["AngleDiffForIntersection"] = 0.25
+param_server_give_way["BehaviorIDMLaneTracking"]["CrosstrackErrorGain"] = 2.5
+param_server_give_way["BehaviorIDMClassic"]["DesiredVelocity"] = 5.
+param_server_give_way["BehaviorIDMClassic"]["MaxLatDifferenceToBeFront"] = 15.0
+param_server_give_way["BehaviorIDMClassic"]["MaxAngleDifferenceToBeFront"] = 3.1
+param_server_give_way["BehaviorIDMClassic"]["MaxLonDifferenceToBeFront"] = 4.0
+param_server_give_way["BehaviorIDMClassic"]["MinimumSpacing"] = 8.0
+
+param_server_take_way["BehaviorIDMLaneTracking"]["CrosstrackErrorGain"] = 2.5
+param_server_take_way["BehaviorIDMClassic"]["DesiredVelocity"] = 5.
+param_server_take_way["BehaviorIDMClassic"]["MaxLatDifferenceToBeFront"] = 0.0
+param_server_take_way["BehaviorIDMClassic"]["MaxAngleDifferenceToBeFront"] = 3.1
+param_server_take_way["BehaviorIDMClassic"]["MaxLonDifferenceToBeFront"] = 0.0
 
 # configure the lane corridors and how the agents 
 lane_corridors = []
 lane_corridors.append(
-  LaneCorridorConfig(params=param_server,
+  LaneCorridorConfig(params=param_server_give_way,
                      source_pos=[-30, -3],
                      sink_pos=[30, -3],
-                     behavior_model=BehaviorIntersectionRuleBased(param_server),
+                     behavior_model=BehaviorIDMClassic(param_server_give_way),
                      min_vel=5.,
                      max_vel=5.,
-                     ds_min=5.,
+                     ds_min=8.,
                      ds_max=10.,
                      s_min=15.,
-                     s_max=30.))
+                     s_max=35.))
 lane_corridors.append(
-  LaneCorridorConfig(params=param_server,
+  LaneCorridorConfig(params=param_server_give_way,
                      source_pos=[30, 3],
                      sink_pos=[-30, 3],
-                     behavior_model=BehaviorIntersectionRuleBased(param_server),
+                     behavior_model=BehaviorIDMClassic(param_server_give_way),
                      min_vel=5.,
                      max_vel=5.,
-                     ds_min=5.,
+                     ds_min=8.,
                      ds_max=10.,
                      s_min=15.,
-                     s_max=30.))
+                     s_max=35.))
 lane_corridors.append(
-  LaneCorridorConfig(params=param_server,
+  LaneCorridorConfig(params=param_server_take_way,
                      source_pos=[3, -30],
                      sink_pos=[-30, 3],
-                     behavior_model=BehaviorIntersectionRuleBased(param_server),
+                     behavior_model=BehaviorIDMClassic(param_server_take_way),
                      controlled_ids=True,
                      min_vel=5.,
                      max_vel=5.,
-                     ds_min=5.,
-                     ds_max=10.,
-                     s_min=15.,
+                     ds_min=10.,
+                     ds_max=12.,
+                     s_min=25.,
                      s_max=30.))
 
 scenarios = \
   ConfigWithEase(num_scenarios=3,
                  map_file_name=Data.xodr_data("threeway_intersection"),
                  random_seed=0,
-                 params=param_server,
+                 params=param_server_give_way,
                  lane_corridor_configs=lane_corridors)
 
 # viewer
-viewer = MPViewer(params=param_server, use_world_bounds=True)
+viewer = MPViewer(params=param_server_give_way, use_world_bounds=True)
 
 # World Simulation
-sim_step_time = param_server["simulation"]["step_time",
+sim_step_time = param_server_give_way["simulation"]["step_time",
                                            "Step-time used in simulation",
                                            0.2]
-sim_real_time_factor = param_server["simulation"]["real_time_factor",
+sim_real_time_factor = param_server_give_way["simulation"]["real_time_factor",
                                                   "execution in real-time or faster",
                                                   1.]
 # viewer = VideoRenderer(renderer=viewer,
